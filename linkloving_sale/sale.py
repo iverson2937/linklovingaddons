4622680f32a3d11a8c4a95b964c11fc94abdc810
Allen <iverson2937@163.com>
2016-11-09 10:20:09 +0800

    update sales module

--- linkloving_sale/sale.py
+++ linkloving_sale/sale.py
@@ -18,17 +18,7 @@ class SaleOrder(models.Model):
             count += line.product_uom_qty
         self.product_count = count
 
-# class SaleOrderLine(models.Model):
-#     _inherit = 'sale.order.line'
-#     tax_id = fields.Many2many(related='order_id.tax_id', store=True)
-#
-#     def default_get(self, cr, uid, ids, context=None):
-#         res = super(SaleOrderLine, self).default_get(cr, uid, ids, context=None)
-#         if context:
-#             context_keys = context.keys()
-#             next_sequence = 1
-#             if 'order_line' in context_keys:
-#                 if len(context.get('order_line')) > 0:
-#                     next_sequence = len(context.get('order_line')) + 1
-#         res.update({'sequence': next_sequence,'tax_id':context.get('default_tax_id')})
-#         return res
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+    tax_id = fields.Many2many(related='order_id.tax_id', store=True)
