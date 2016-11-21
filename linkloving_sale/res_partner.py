e859604e7dd87b72204699cd2e57e5825dfc1cd5
Allen <iverson2937@163.com>
2016-11-21 14:57:23 +0800

    change str to u

--- linkloving_sale/res_partner.py
+++ linkloving_sale/res_partner.py
@@ -18,12 +18,12 @@ class Partner(models.Model):
     team_id = fields.Many2one('crm.team', default=_get_default_team)
     source_id = fields.Many2one('res.partner.source')
     level = fields.Selection([
-        (1, '1级'),
-        (2, '2级'),
-        (3, '3级')
-    ], string='客户等级', default=1)
+        (1, u'1级'),
+        (2, u'2级'),
+        (3, u'3级')
+    ], string=u'客户等级', default=1)
     internal_code = fields.Char(string='No')
-    x_qq = fields.Char(string='即时通信')
+    x_qq = fields.Char(string=u'即时通信')
 
     _sql_constraints = [
         ('internal_code_uniq', 'unique (internal_code)', 'The No must be unique!')
