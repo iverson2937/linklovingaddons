6a39cb3a7995c0a69fedb913a50cf9714d381853
Allen <iverson2937@163.com>
2016-11-23 10:57:31 +0800

    fix tax issue for purchase

--- linkloving_purchase/models/purchase_order.py
+++ linkloving_purchase/models/purchase_order.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 import logging
-import threading
+from datetime import datetime
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
+from odoo import models, fields, api, _, SUPERUSER_ID
 
-from openerp import models, fields, api, _, SUPERUSER_ID
-from openerp import tools
-from openerp.osv import osv
 
 _logger = logging.getLogger(__name__)
 
@@ -16,6 +15,12 @@ class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
     product_count = fields.Float(compute='get_product_count')
+    tax_id = fields.Many2one('account.tax', string='Tax')
+
+    @api.onchange('tax_id')
+    def onchange_tax_id(self):
+        for line in self.order_line:
+            line.tax_ids = [(6, 0, [self.tax_id.id])]
 
     def get_product_count(self):
         count = 0.0
@@ -27,3 +32,38 @@ class PurchaseOrder(models.Model):
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
     po_id = fields.Many2one('purchase.order')
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+    # 重写默认税的选择
+    @api.onchange('product_id')
+    def onchange_product_id(self):
+        result = {}
+        if not self.product_id:
+            return result
+
+        # Reset date, price and quantity since _onchange_quantity will provide default values
+        self.date_planned = datetime.today().strftime(DEFAULT_SERVER_DATETIME_FORMAT)
+        self.price_unit = self.product_qty = 0.0
+        self.product_uom = self.product_id.uom_po_id or self.product_id.uom_id
+        result['domain'] = {'product_uom': [('category_id', '=', self.product_id.uom_id.category_id.id)]}
+
+        product_lang = self.product_id.with_context({
+            'lang': self.partner_id.lang,
+            'partner_id': self.partner_id.id,
+        })
+        self.name = product_lang.display_name
+        if product_lang.description_purchase:
+            self.name += '\n' + product_lang.description_purchase
+
+        fpos = self.order_id.fiscal_position_id
+
+        self.taxes_id =self.order_id.tax_id
+        print self.taxes_id,'eeeeeeeeeeeeeee'
+
+
+
+        self._suggest_quantity()
+        self._onchange_quantity()
+
+        return result
