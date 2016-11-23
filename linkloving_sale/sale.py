12f797e52b8983138b8a0308ae6a4fc7df8397ae
Allen <iverson2937@163.com>
2016-11-23 10:33:04 +0800

    fix sale order tax issue

--- linkloving_sale/sale.py
+++ linkloving_sale/sale.py
@@ -11,6 +11,7 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
     tax_id = fields.Many2one('account.tax', required=True)
     product_count = fields.Float(compute='get_product_count')
+    pi_number = fields.Char(string='PI Number')
 
     def get_product_count(self):
         count = 0.0
