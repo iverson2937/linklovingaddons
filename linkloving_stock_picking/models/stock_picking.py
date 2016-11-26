b591ac022668f3a713dc9e35dda8090d8736d792
allen <iverson2937@163.com>
2016-11-26 21:24:18 +0800

    更新入库流程

--- linkloving_stock_picking/models/stock_picking.py
+++ linkloving_stock_picking/models/stock_picking.py
@@ -12,9 +12,11 @@ class StockPicking(models.Model):
         ('waiting', 'Waiting Another Operation'),
         ('confirmed', 'Waiting Availability'),
         ('partially_available', 'Partially Available'),
-        ('post',u'入库'),
+        ('assigned', 'Available'),
+        ('post', u'入库'),
         ('qc_check', u'品检'),
-        ('assigned', 'Available'), ('done', 'Done')], string='Status', compute='_compute_state',
+        ('validate', u'调拨'),
+        ('done', 'Done')], string='Status', compute='_compute_state',
         copy=False, index=True, readonly=True, store=True, track_visibility='onchange',
         help=" * Draft: not confirmed yet and will not be scheduled until confirmed\n"
              " * Waiting Another Operation: waiting for another move to proceed before it becomes automatically available (e.g. in Make-To-Order flows)\n"
@@ -24,49 +26,6 @@ class StockPicking(models.Model):
              " * Transferred: has been processed, can't be modified or cancelled anymore\n"
              " * Cancelled: has been cancelled, can't be confirmed anymore")
 
-    @api.depends('move_type', 'launch_pack_operations', 'move_lines.state', 'move_lines.picking_id', 'move_lines.partially_available')
-    @api.one
-    def _compute_state(self):
-        ''' State of a picking depends on the state of its related stock.move
-         - no moves: draft or assigned (launch_pack_operations)
-         - all moves canceled: cancel
-         - all moves done (including possible canceled): done
-         - All at once picking: least of confirmed / waiting / assigned
-         - Partial picking
-          - all moves assigned: assigned
-          - one of the move is assigned or partially available: partially available
-          - otherwise in waiting or confirmed state
-        '''
-        if not self.move_lines and self.launch_pack_operations:
-            self.state = 'draft'
-        elif not self.move_lines:
-            self.state = 'draft'
-        elif any(move.state == 'draft' for move in self.move_lines):  # TDE FIXME: should be all ?
-            self.state = 'draft'
-        elif all(move.state == 'cancel' for move in self.move_lines):
-            self.state = 'cancel'
-        elif all(move.state in ['cancel', 'done'] for move in self.move_lines):
-            self.state = 'done'
-        elif self.move_type == 'one':
-            ordered_moves = self.move_lines.filtered(
-                lambda move: move.state not in ['cancel', 'done']
-            ).sorted(
-                key=lambda move: (move.state == 'assigned' and 2) or (move.state == 'waiting' and 1) or 0, reverse=False
-            )
-            self.state = ordered_moves[0].state
-        else:
-            filtered_moves = self.move_lines.filtered(lambda move: move.state not in ['cancel', 'done'])
-            if not all(move.state == 'assigned' for move in filtered_moves) and any(move.state == 'assigned' for move in filtered_moves):
-                self.state = 'partially_available'
-            elif any(move.partially_available for move in filtered_moves):
-                self.state = 'partially_available'
-            else:
-                if self.picking_type_code=='incoming':
-                    self.state='post'
-                else:
-                    ordered_moves = filtered_moves.sorted(key=lambda move: (move.state == 'assigned' and 2) or (move.state == 'waiting' and 1) or 0, reverse=True)
-                    self.state = ordered_moves[0].state
-        print self.state
 
     @api.multi
     def action_post(self):
@@ -74,13 +33,12 @@ class StockPicking(models.Model):
 
     @api.multi
     def action_check_pass(self):
-        self.state='assigned'
+        self.state = 'validate'
 
     @api.multi
     def action_check_fail(self):
-        self.state='post'
+        self.state = 'assigned'
 
     @api.multi
     def reject(self):
-        self.state = 'post'
-
+        self.state = 'assigned'
