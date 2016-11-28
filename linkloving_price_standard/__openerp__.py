349f5df4321a86a4b6b4679fda35f6f1fd1a2be8
Allen <iverson2937@163.com>
2016-11-28 11:00:44 +0800

    fixbug

--- linkloving_price_standard/__openerp__.py
+++ linkloving_price_standard/__openerp__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','product','sale'],
+    'depends': ['base', 'product', 'sale', 'linkloving_sale'],
 
     # always loaded
     'data': [
