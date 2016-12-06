542c930a2ceef81828b5b0c6e183c30a6b89f556
Allen <iverson2937@163.com>
2016-12-06 14:57:44 +0800

    让销售看到bom

--- linkloving_mrp_extend/__manifest__.py
+++ linkloving_mrp_extend/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','mrp'],
+    'depends': ['base','mrp','sales_team'],
 
     # always loaded
     'data': [
