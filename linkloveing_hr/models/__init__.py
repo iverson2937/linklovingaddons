5c79b9d16055057f8bb181177d81c2f00e4e2f4e
Allen <iverson2937@163.com>
2016-12-08 18:32:37 +0800

    更新暂支

--- linkloveing_hr/models/__init__.py
+++ linkloveing_hr/models/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import hr_expense, mail_thread, hr_department
+from . import hr_expense, mail_thread, hr_department,account_payment
---
+++ linkloveing_hr/models/account_payment.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+
+
+class AccountPayment(models.Model):
+    _inherit = 'account.payment'
+
+    partner_type = fields.Selection(selection_add=[('employee', 'employee')])
+
