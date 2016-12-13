63b1e7e259c0e1e5c20b6f8df6b0c0946440536e
allen <iverson2937@163.com>
2016-12-14 00:51:50 +0800

    update expense

--- linkloveing_hr/__openerp__.py
+++ linkloveing_hr/__openerp__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale', 'hr_expense','account'],
+    'depends': ['base', 'sale', 'hr_expense', 'account', 'purchase'],
 
     # always loaded
     'data': [
@@ -32,7 +32,8 @@
         'view/hr_expense_views.xml',
         'view/hr_department_view.xml',
         'view/partner_view.xml',
-        'view/hr_view.xml',
+        'view/hr_expense_sheet_views.xml',
+        'view/hr_employee.xml',
         'wizard/hr_expense_register_payment.xml'
     ],
     # only loaded in demonstration mode
--- linkloveing_hr/models/account_payment.py
+++ linkloveing_hr/models/account_payment.py
@@ -5,12 +5,21 @@ from odoo.exceptions import UserError
 
 
 class AccountPayment(models.Model):
+
     _inherit = 'account.payment'
 
     partner_type = fields.Selection(selection_add=[('employee', 'employee')])
+    to_approve_id = fields.Many2one('res.users')
+
+    def _get_default_partner(self):
+        return self.env.user.partner_id.id
+
+    partner_id = fields.Many2one('res.partner', default=_get_default_partner)
+
 
     approve_ids = fields.Many2many('res.users')
     state = fields.Selection([('draft', 'Draft'),
+                              ('confirm', u'确认'),
                               ('manager1_approve', u'一级审批'),
                               ('manager2_approve', u'二级审批'),
                               ('manager3_approve', u'总经理审批'),
@@ -36,6 +45,20 @@ class AccountPayment(models.Model):
         return res
 
     @api.multi
+    def submit(self):
+        self.state = 'confirm'
+        employee_id = self.partner_id.user_ids.employee_ids
+        if employee_id == employee_id.department_id.manager_id:
+            department = self.to_approve_id.employee_ids.department_id
+            if department.allow_amount and self.total_amount > department.allow_amount:
+                self.write({'state': 'approve'})
+            else:
+                self.to_approve_id = self.employee_id.department_id.parent_id.manager_id.user_id.id
+        else:
+            self.to_approve_id = employee_id.department_id.manager_id.user_id.id
+
+
+    @api.multi
     def manager1_approve(self):
         # if self.employee_id == self.employee_id.department_id.manager_id:
         #     self.to_approve_id = self.employee_id.department_id.parent_id.manager_id.user_id.id
@@ -51,9 +74,14 @@ class AccountPayment(models.Model):
             self.write({'state': 'manager1_approve', 'approve_ids': [(4, self.env.user.id)]})
 
     @api.multi
+    def reject(self):
+        self.state = 'draft'
+        self.to_approve_id = False
+
+    @api.multi
     def manager2_approve(self):
         department = self.to_approve_id.employee_ids.department_id
-        if department.allow_amount and self.total_amount < department.allow_amount:
+        if self.amount < department.allow_amount:
             self.to_approve_id = False
             self.write({'state': 'approve', 'approve_ids': [(4, self.env.user.id)]})
 
@@ -67,16 +95,4 @@ class AccountPayment(models.Model):
 
         self.write({'state': 'approve', 'approve_ids': [(4, self.env.user.id)]})
 
-    @api.model
-    def create(self, vals):
 
-        payment = super(AccountPayment, self).create(vals)
-        if payment.employee_id == payment.employee_id.department_id.manager_id:
-            department = payment.to_approve_id.employee_ids.department_id
-            if department.allow_amount and self.total_amount > department.allow_amount:
-                payment.write({'state': 'approve'})
-            else:
-                payment.to_approve_id = payment.employee_id.department_id.parent_id.manager_id.user_id.id
-        else:
-            payment.to_approve_id = payment.employee_id.department_id.manager_id.user_id.id
-        return payment
