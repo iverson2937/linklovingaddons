897f06dabe93c0810e6b76e4679e5a74a854eee1
Allen <iverson2937@163.com>
2016-12-30 12:54:07 +0800

    update fiscal year

--- linkloving_account_close/models/account_move.py
+++ linkloving_account_close/models/account_move.py
@@ -11,18 +11,17 @@ class AccountMoveLine(models.Model):
 
     period_id = fields.Many2one('account.period', string=u'会计区间',
                                 domain=[('state', '!=', 'done')], copy=False,
+                                required=True,
                                 help="Keep empty to use the period of the validation(invoice) date.",
                                 readonly=False)
+    fiscalyear_id = fields.Many2one('account.fiscalyear')
 
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    def _get_period(self):
-        period_ids = self.env('account.period').search([('state', '!=', 'done')])
-        return period_ids[0]
-
     period_id = fields.Many2one('account.period', string=u'会计区间',
                                 domain=[('state', '!=', 'done')], copy=False,
+                                required=True,
                                 help="Keep empty to use the period of the validation(invoice) date.",
-                                default=_get_period)
+                                )
