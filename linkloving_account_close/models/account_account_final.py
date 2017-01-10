752fbea2aaee2c36f7d128a9a63c32b8ce409632
Allen <iverson2937@163.com>
2017-01-10 14:52:43 +0800

    update account

--- linkloving_account_close/models/account_account_final.py
+++ linkloving_account_close/models/account_account_final.py
@@ -7,18 +7,17 @@ class AccountAccountFinal(models.Model):
     _name = "account.account.final"
     _description = "Final Account"
     _order = "create_date desc"
-    name = fields.Char('Fiscal Year')
-    account_id = fields.Many2one('account.account',string='科目')
+    account_id = fields.Many2one('account.account', string='科目')
     partner_id = fields.Many2one('res.partner')
-    period_id = fields.Many2one('account.period',string='会计区间')
+    period_id = fields.Many2one('account.period', string='会计区间')
+    fiscalyear_id = fields.Many2one('account.fiscalyear', related='period_id.fiscalyear_id', store=True)
     company_id = fields.Many2one('res.company', 'Company', required=True,
                                  default=lambda self: self.env.user.company_id.id)
-    # start_debit = fields.Monetary(default=0.0)
-    # start_credit = fields.Monetary(default=0.0)
-    # debit = fields.Monetary(default=0.0)
-    # credit = fields.Monetary(default=0.0)
-    # year_debit = fields.Monetary(default=0.0)
-    # year_credit = fields.Monetary(default=0.0)
-    # end_debit = fields.Monetary(default=0.0)
-    # end_credit = fields.Monetary(default=0.0)
-
+    start_debit = fields.Monetary(default=0.0)
+    start_credit = fields.Monetary(default=0.0)
+    debit = fields.Monetary(default=0.0)
+    credit = fields.Monetary(default=0.0)
+    year_debit = fields.Monetary(default=0.0)
+    year_credit = fields.Monetary(default=0.0)
+    end_debit = fields.Monetary(default=0.0)
+    end_credit = fields.Monetary(default=0.0)
