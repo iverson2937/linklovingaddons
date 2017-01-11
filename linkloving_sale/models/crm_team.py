3f771bc8c75d362ba991992423edcb8dab78d964
Allen <iverson2937@163.com>
2017-01-11 21:20:16 +0800

    update sales report

--- linkloving_sale/models/crm_team.py
+++ linkloving_sale/models/crm_team.py
@@ -9,3 +9,5 @@ from odoo import fields, api, models
 class CrmTeam(models.Model):
     _inherit = "crm.team"
     code = fields.Char(string=u'简码')
+    #for inner_code
+    is_domestic=fields.Boolean(string=u'是否国内团队')
--- linkloving_sale/models/sale.py
+++ linkloving_sale/models/sale.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import fields, models, api, _, SUPERUSER_ID
-
+from itertools import groupby
 
 class SaleOrder(models.Model):
     """
@@ -12,6 +12,7 @@ class SaleOrder(models.Model):
     tax_id = fields.Many2one('account.tax', required=True)
     product_count = fields.Float(compute='get_product_count')
     pi_number = fields.Char(string='PI Number')
+
 
     def get_product_count(self):
         count = 0.0
@@ -28,6 +29,30 @@ class SaleOrder(models.Model):
         result = super(SaleOrder, self).write(vals)
         self.mapped('order_line')._compute_amount()
         return result
+
+    @api.multi
+    def order_lines_layouted(self):
+        """
+        Returns this order lines classified by sale_layout_category and separated in
+        pages according to the category pagebreaks. Used to render the report.
+        """
+        self.ensure_one()
+        report_pages = [[]]
+        for category, lines in groupby(self.order_line, lambda l: l.layout_category_id):
+            # If last added category induced a pagebreak, this one will be on a new page
+            if report_pages[-1] and report_pages[-1][-1]['pagebreak']:
+                report_pages.append([])
+            # Append category to current report page
+            report_pages[-1].append({
+                'name': category and category.name or 'Uncategorized',
+                'subtotal': category and category.subtotal,
+                'pagebreak': category and category.pagebreak,
+                'lines': list(lines),
+                'is_domestic':self.team_id.is_domestic if self.team_id else False
+            })
+        print report_pages
+
+        return report_pages
 
 
 class SaleOrderLine(models.Model):
