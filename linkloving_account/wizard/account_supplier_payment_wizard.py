42a51c637aebca8f18217c67711e8b7f6c9a8c45
Allen <iverson2937@163.com>
2017-02-04 14:12:00 +0800

    fix bug

--- linkloving_account/wizard/account_supplier_payment_wizard.py
+++ linkloving_account/wizard/account_supplier_payment_wizard.py
@@ -64,7 +64,7 @@ class AccountEmployeeRegisterPaymentWizard(models.TransientModel):
         self.ensure_one()
         context = dict(self._context or {})
         active_ids = context.get('active_ids', [])
-        expense_sheet = self.env['hr.expense.sheet'].browse(active_ids)
+        # expense_sheet = self.env['hr.expense.sheet'].browse(active_ids)
 
         # Create payment and post it
         payment = self.env['account.payment'].create({
@@ -81,19 +81,19 @@ class AccountEmployeeRegisterPaymentWizard(models.TransientModel):
         })
         payment.post()
 
-        # Log the payment in the chatter
-        body = (_(
-            "A payment of %s %s with the reference <a href='/mail/view?%s'>%s</a> related to your expense %s has been made.") % (
-                    payment.amount, payment.currency_id.symbol,
-                    url_encode({'model': 'account.payment', 'res_id': payment.id}), payment.name, expense_sheet.name))
-        expense_sheet.message_post(body=body)
+        # # Log the payment in the chatter
+        # body = (_(
+        #     "A payment of %s %s with the reference <a href='/mail/view?%s'>%s</a> related to your expense %s has been made.") % (
+        #             payment.amount, payment.currency_id.symbol,
+        #             url_encode({'model': 'account.payment', 'res_id': payment.id}), payment.name, expense_sheet.name))
+        # expense_sheet.message_post(body=body)
 
         # Reconcile the payment and the expense, i.e. lookup on the payable account move lines
-        account_move_lines_to_reconcile = self.env['account.move.line']
-        for line in payment.move_line_ids + expense_sheet.account_move_id.line_ids:
-            if line.account_id.internal_type == 'payable':
-                account_move_lines_to_reconcile |= line
-        account_move_lines_to_reconcile.reconcile()
+        # account_move_lines_to_reconcile = self.env['account.move.line']
+        # for line in payment.move_line_ids + expense_sheet.account_move_id.line_ids:
+        #     if line.account_id.internal_type == 'payable':
+        #         account_move_lines_to_reconcile |= line
+        # account_move_lines_to_reconcile.reconcile()
 
         self.register_id.state='done'
 
