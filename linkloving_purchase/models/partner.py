48402cf52a27adbc89d86e5dee78cacd60e9fe7b
Allen <iverson2937@163.com>
2017-02-04 11:44:55 +0800

    显示付款申请数量

--- linkloving_purchase/models/partner.py
+++ linkloving_purchase/models/partner.py
@@ -10,6 +10,13 @@ class Partner(models.Model):
     #                             compute='_compute_company_type', readonly=False, )
     is_company = fields.Boolean(string='Is a Company', default=True,
                                 help="Check if the contact is a company, otherwise it is a person")
+    payment_count = fields.Integer(compute='_compute_payment_count', string='# Payment')
+    payment_ids = fields.One2many('account.payment.register', 'partner_id')
+
+    def _compute_payment_count(self):
+        for partner in self:
+            partner.payment_count = len(partner.payment_ids)
+
 
 class Company(models.Model):
     _inherit = 'res.company'
