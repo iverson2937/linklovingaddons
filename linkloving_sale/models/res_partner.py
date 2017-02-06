08a53fa5417e2fcc1b4ab03c0c2eb9d015459f99
Allen <iverson2937@163.com>
2017-02-06 15:20:06 +0800

    fix 默认respartner 为公司的bug

--- linkloving_sale/models/res_partner.py
+++ linkloving_sale/models/res_partner.py
@@ -41,6 +41,31 @@ class Partner(models.Model):
             res.internal_code = internal_code
         return res
 
+    @api.multi
+    def name_get(self):
+        res = []
+        for partner in self:
+            name = partner.name or ''
+
+            if partner.company_name or partner.parent_id:
+                if not name and partner.type in ['invoice', 'delivery', 'other']:
+                    name = dict(self.fields_get(['type'])['type']['selection'])[partner.type]
+                if not partner.is_company:
+                    name = "%s, %s" % (partner.commercial_company_name or partner.parent_id.name, name)
+            if self._context.get('show_address_only'):
+                name = partner._display_address(without_company=True)
+            if self._context.get('show_address'):
+                name = name + "\n" + partner._display_address(without_company=True)
+            name = name.replace('\n\n', '\n')
+            name = name.replace('\n\n', '\n')
+            if self._context.get('show_email') and partner.email:
+                name = "%s <%s>" % (name, partner.email)
+            if self._context.get('html_format'):
+                name = name.replace('\n', '<br/>')
+            res.append((partner.id, name))
+        print res
+        return res
+
 
 class ResPartnerSource(models.Model):
     """
