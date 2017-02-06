6ea855ae66cba40d85055983113b9cdeeea74e43
chary1n <357346640@qq.com>
2017-02-06 11:20:58 +0800

    上传打印log 代码

--- linkloving_qrcode_create/models/models.py
+++ linkloving_qrcode_create/models/models.py
@@ -114,8 +114,10 @@ class ProductTemplateExtend(models.Model):
     def cur_file_dir(cls):
         # 获取脚本路径
         path = sys.path[0]
+        _logger.warning(path)
         # 判断为脚本文件还是py2exe编译后的文件，如果是脚本文件，则返回的是脚本的目录，如果是py2exe编译后的文件，则返回的是编译后的文件路径
-        if os.path.isdir(path):
-            return path
-        elif os.path.isfile(path):
-            return os.path.dirname(path)
+        return path
+        # if os.path.isdir(path):
+        #     return path
+        # elif os.path.isfile(path):
+        #     return os.path.dirname(path)
