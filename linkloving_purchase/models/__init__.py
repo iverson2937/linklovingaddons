dde9586c9fdaf4a92e210d1195f7b78c28b7c4dc
Allen <iverson2937@163.com>
2017-02-07 13:48:36 +0800

    批量取消saleorder

--- linkloving_purchase/models/__init__.py
+++ linkloving_purchase/models/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import res_company,partner, qualifications_level, purchase_order,purchase_config_settings
+from . import res_company,partner, qualifications_level, purchase_order,purchase_config_settings,purchase_order_cancel
