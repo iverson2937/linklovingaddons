04c7916d6b065a83265104121c0e952874d4697b
Allen <iverson2937@163.com>
2017-02-08 12:00:31 +0800

    fix 取消问题

---
+++ linkloving_warehouse/models/stock_move.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright 2015 Eezee-It, MONK Software, Vauxoo
+#    Copyright 2013 Camptocamp
+#    Copyright 2009-2013 Akretion,
+#    Author: Emmanuel Samyn, Raphaël Valyi, Sébastien Beau,
+#            Benoît Guillot, Joel Grand-Guillaume, Leonardo Donelli
+#            Osval Reyes, Yanina Aular
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+from odoo import fields, models,api,_
+from odoo.exceptions import UserError
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.move'
+
+    @api.multi
+    def action_cancel(self):
+        """ Cancels the moves and if all moves are cancelled it cancels the picking. """
+        # TDE DUMB: why is cancel_procuremetn in ctx we do quite nothing ?? like not updating the move ??
+        if any(move.state == 'done' for move in self):
+            raise UserError(_('You cannot cancel a stock move that has been set to \'Done\'.'))
+
+        procurements = self.env['procurement.order']
+        for move in self:
+            if move.reserved_quant_ids:
+                move.quants_unreserve()
+            if self.env.context.get('cancel_procurement'):
+                if move.propagate:
+                    pass
+                    # procurements.search([('move_dest_id', '=', move.id)]).cancel()
+            else:
+                if move.move_dest_id:
+                    if move.propagate and move.move_dest_id.state!='done':
+                        move.move_dest_id.action_cancel()
+                    elif move.move_dest_id.state == 'waiting':
+                        # If waiting, the chain will be broken and we are not sure if we can still wait for it (=> could take from stock instead)
+                        move.move_dest_id.write({'state': 'confirmed'})
+                if move.procurement_id:
+                    procurements |= move.procurement_id
+
+        self.write({'state': 'cancel', 'move_dest_id': False})
+        if procurements:
+            procurements.check()
+        return True
