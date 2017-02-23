ea429d75891ec3abcaccde44d86c861ce478c3bd
Allen <iverson2937@163.com>
2017-02-23 15:32:42 +0800

    更新成本计算

--- linkloving_process/models/mrp_bom.py
+++ linkloving_process/models/mrp_bom.py
@@ -13,3 +13,18 @@ class MrpProduction(models.Model):
         ('unit', u'计件计价'),
         ('time', u'计时计价'),
     ], default='unit')
+    cycle_time = fields.Integer(string=u'理论工时')
+
+    @api.depends('cost', 'hour_price')
+    def _get_product_cost(self):
+        self.cost = (float(self.cycle_time) / 3600) * self.hour_price
+
+    cost = fields.Monetary(string=u'成本', compute=_get_product_cost,currency_field='currency_id')
+    currency_id = fields.Many2one('res.currency', string='Currency', required=True,
+                                  default=lambda self: self.env.user.company_id.currency_id)
+    hour_price = fields.Float(string=u'时薪')
+    @api.onchange('process_id')
+    def on_change_price(self):
+        self.hour_price = self.process_id.hour_price
+
+
--- linkloving_process/models/mrp_process.py
+++ linkloving_process/models/mrp_process.py
@@ -7,5 +7,6 @@ class MrpProcess(models.Model):
     _name = 'mrp.process'
     name = fields.Char(string=u'名称')
     description = fields.Text(string=u'描述')
-    unit_price = fields.Float(string=u'单价')
+    unit_price = fields.Float(string=u'计件单价')
+    hour_price = fields.Float(string=u'时薪')
     partner_id = fields.Many2one('res.partner', string=u'负责人')
