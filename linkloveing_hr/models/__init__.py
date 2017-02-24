7b4a387ada3ef01c3c1346821a163a90ec8c14ee
Allen <iverson2937@163.com>
2017-02-24 11:04:08 +0800

    自动判断是否为员工

--- linkloveing_hr/models/__init__.py
+++ linkloveing_hr/models/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
 
-from . import account_employee_payment,hr_expense, mail_thread, hr_department, account_employee_payment, account_payment, ir_menu, hr_employee, \
-    hr_expense_sheet,account_employee_payment_return
-
+from . import account_employee_payment, hr_expense, mail_thread, hr_department, account_employee_payment, \
+    account_payment, ir_menu, hr_employee, hr_expense_sheet, account_employee_payment_return, res_partner
--- linkloveing_hr/models/res_partner.py
+++ linkloveing_hr/models/res_partner.py
@@ -6,3 +6,11 @@ from odoo.exceptions import UserError
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
+    @api.one
+    @api.depends('employee_ids')
+    def is_employee(self):
+        if self.employee_ids:
+            self.employee = True
+
+    employee = fields.Boolean(compute='is_employee',store=True)
+    employee_ids = fields.One2many('hr.employee', 'address_home_id')
