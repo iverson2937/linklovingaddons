985850bcba2bf0dc997cd55f4a0008db8134fc9e
Allen <iverson2937@163.com>
2017-02-28 09:10:34 +0800

    fix mail thread bug

--- linkloveing_hr/models/mail_thread.py
+++ linkloveing_hr/models/mail_thread.py
@@ -19,3 +19,28 @@ class MailThread(models.AbstractModel):
             user_ids = [self._uid]
         return self.message_subscribe(self.env['res.users'].sudo().browse(user_ids).mapped('partner_id').ids,
                                       subtype_ids=subtype_ids)
+
+    @api.multi
+    def _get_message_unread(self):
+        if not self.ids:
+            return
+        res = dict((res_id, 0) for res_id in self.ids)
+        partner_id = self.env.user.partner_id.id
+
+        # search for unread messages, directly in SQL to improve performances
+        self._cr.execute(""" SELECT msg.res_id FROM mail_message msg
+                             RIGHT JOIN mail_message_mail_channel_rel rel
+                             ON rel.mail_message_id = msg.id
+                             RIGHT JOIN mail_channel_partner cp
+                             ON (cp.channel_id = rel.mail_channel_id AND cp.partner_id = %s AND
+                                (cp.seen_message_id IS NULL OR cp.seen_message_id < msg.id))
+                             WHERE msg.model = %s AND msg.res_id in %s AND
+                                   (msg.author_id IS NULL OR msg.author_id != %s) AND
+                                   (msg.message_type != 'notification' OR msg.model != 'mail.channel')""",
+                         (partner_id, self._name, tuple(self.ids), partner_id,))
+        for result in self._cr.fetchall():
+            res[result[0]] += 1
+
+        for record in self:
+            record.message_unread_counter = res.get(record.id, 0)
+            record.message_unread = bool(record.message_unread_counter)
