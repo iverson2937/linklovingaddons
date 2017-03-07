4c89217a56c3f833a7d788d3783ba1136bc4ffe5
Allen <iverson2937@163.com>
2017-03-07 20:41:10 +0800

    update process

--- linkloving_process/__manifest__.py
+++ linkloving_process/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'mrp','linkloving_account'],
+    'depends': ['base', 'mrp', 'linkloving_account'],
 
     # always loaded
     'data': [
@@ -29,6 +29,7 @@
         'views/mrp_process.xml',
         'views/mrp_bom.xml',
         'views/mrp_production.xml',
+        'views/res_partner.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
