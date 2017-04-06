eea92d44bee76053619be00aa601b1efc4249589
Allen <iverson2937@163.com>
2017-04-06 15:07:25 +0800

    添加mo源单据号

--- linkloving_warehouse/models/__init__.py
+++ linkloving_warehouse/models/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import location, product, mrp_production_cancel, stock_move, mrp_product_combine
+from . import location, product, mrp_production_cancel, stock_move, mrp_product_combine, mrp_production
