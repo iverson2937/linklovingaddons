a7e1ef61000ea3f64a701ba902fcaf225a698d6d
Allen <iverson2937@163.com>
2017-04-06 19:12:32 +0800

    完善退货

--- linkloving_return_goods/__manifest__.py
+++ linkloving_return_goods/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale', 'purchase', 'stock'],
+    'depends': ['base', 'sale', 'purchase', 'stock', 'linkloving_invoice_workflow'],
 
     # always loaded
     'data': [
