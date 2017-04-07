cf9b648f575e9acbfdd1e4d134c847e8eb3ff324
Allen <iverson2937@163.com>
2017-04-07 20:31:24 +0800

    返工

--- linkloving_mrp_extend/models/models.py
+++ linkloving_mrp_extend/models/models.py
@@ -8,7 +8,7 @@ import jpush
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
 from odoo.osv import expression
-from odoo.tools import float_compare
+from odoo.tools import float_compare, math
 from odoo.addons import decimal_precision as dp
 class MrpBomExtend(models.Model):
     _inherit = 'mrp.bom'
@@ -380,6 +380,7 @@ class MrpProductionExtend(models.Model):
 
     @api.multi
     def _generate_raw_move(self, bom_line, line_data):
+
         quantity = line_data['qty']
         # alt_op needed for the case when you explode phantom bom and all the lines will be consumed in the operation given by the parent bom line
         alt_op = line_data['parent_line'] and line_data['parent_line'].operation_id.id or False
@@ -414,6 +415,24 @@ class MrpProductionExtend(models.Model):
             'suggest_qty':  line_data['suggest_qty'],
         }
         return self.env['stock.move'].create(data)
+
+    @api.multi
+    def update_rework_material(self, line, qty):
+        quantity = line['product_qty']
+        self.ensure_one()
+        move = self.move_raw_ids.filtered(
+            lambda x: x.product_id.id == line.product_id.id and x.state not in ('done', 'cancel'))
+        if move:
+            if quantity > 0:
+                move[0].write({'product_uom_qty': quantity * qty,
+                               'suggest_qty': quantity * qty})
+            else:
+                if move[0].quantity_done > 0:
+                    raise UserError(_(
+                        'Lines need to be deleted, but can not as you still have some quantities to consume in them. '))
+                move[0].action_cancel()
+                move[0].unlink()
+            return move
 
     @api.multi
     def _update_raw_move(self, bom_line, line_data):
@@ -451,6 +470,69 @@ class MrpProductionExtend(models.Model):
 
 class ChangeProductionQty(models.TransientModel):
     _inherit = 'change.production.qty'
+
+    @api.multi
+    def change_prod_qty(self):
+        for wizard in self:
+            production = wizard.mo_id
+            produced = sum(production.move_finished_ids.mapped('quantity_done'))
+            if wizard.product_qty < produced:
+                raise UserError(
+                    _("You have already processed %d. Please input a quantity higher than %d ") % (produced, produced))
+            production.write({'product_qty': wizard.product_qty})
+            done_moves = production.move_finished_ids.filtered(
+                lambda x: x.state == 'done' and x.product_id == production.product_id)
+            qty_produced = production.product_id.uom_id._compute_quantity(sum(done_moves.mapped('product_qty')),
+                                                                          production.product_uom_id)
+            factor = production.product_uom_id._compute_quantity(production.product_qty - qty_produced,
+                                                                 production.bom_id.product_uom_id) / production.bom_id.product_qty
+            boms, lines = production.bom_id.explode(production.product_id, factor,
+                                                    picking_type=production.bom_id.picking_type_id)
+            if production.is_rework:
+                for line in production.rework_material_line_ids:
+                    production.update_rework_material(line, production.product_qty)
+
+            else:
+                for line, line_data in lines:
+                    production._update_raw_move(line, line_data)
+            operation_bom_qty = {}
+            for bom, bom_data in boms:
+                for operation in bom.routing_id.operation_ids:
+                    operation_bom_qty[operation.id] = bom_data['qty']
+            self._update_product_to_produce(production, production.product_qty - qty_produced)
+            moves = production.move_raw_ids.filtered(lambda x: x.state not in ('done', 'cancel'))
+            moves.do_unreserve()
+            moves.action_assign()
+            for wo in production.workorder_ids:
+                operation = wo.operation_id
+                if operation_bom_qty.get(operation.id):
+                    cycle_number = math.ceil(
+                        operation_bom_qty[operation.id] / operation.workcenter_id.capacity)  # TODO: float_round UP
+                    wo.duration_expected = (operation.workcenter_id.time_start +
+                                            operation.workcenter_id.time_stop +
+                                            cycle_number * operation.time_cycle * 100.0 / operation.workcenter_id.time_efficiency)
+                if production.product_id.tracking == 'serial':
+                    quantity = 1.0
+                else:
+                    quantity = wo.qty_production - wo.qty_produced
+                    quantity = quantity if (quantity > 0) else 0
+                wo.qty_producing = quantity
+                if wo.qty_produced < wo.qty_production and wo.state == 'done':
+                    wo.state = 'progress'
+                # assign moves; last operation receive all unassigned moves
+                # TODO: following could be put in a function as it is similar as code in _workorders_create
+                # TODO: only needed when creating new moves
+                moves_raw = production.move_raw_ids.filtered(
+                    lambda move: move.operation_id == operation and move.state not in ('done', 'cancel'))
+                if wo == production.workorder_ids[-1]:
+                    moves_raw |= production.move_raw_ids.filtered(lambda move: not move.operation_id)
+                moves_finished = production.move_finished_ids.filtered(
+                    lambda move: move.operation_id == operation)  # TODO: code does nothing, unless maybe by_products?
+                moves_raw.mapped('move_lot_ids').write({'workorder_id': wo.id})
+                (moves_finished + moves_raw).write({'workorder_id': wo.id})
+                if wo.move_raw_ids.filtered(lambda x: x.product_id.tracking != 'none') and not wo.active_move_lot_ids:
+                    wo._generate_lot_ids()
+        return {}
 
     # @api.multi
     # def change_prod_qty(self):
--- linkloving_rework/models/mrp_production.py
+++ linkloving_rework/models/mrp_production.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 
 class MrpProduction(models.Model):
@@ -12,3 +13,36 @@ class MrpProduction(models.Model):
     def onchange_process_id(self):
         if self.process_id.is_rework:
             pass
+
+    def _generate_raw_moves(self, exploded_lines):
+        self.ensure_one()
+        moves = self.env['stock.move']
+        source_location = self.picking_type_id.default_location_src_id
+        if self.process_id.is_rework:
+            if not self.rework_material_line_ids:
+                raise UserError(u'请添加重工物料')
+            for line_id in self.rework_material_line_ids:
+                data = {
+                    'name': self.name,
+                    'date': self.date_planned_start,
+                    'product_id': line_id.product_id.id,
+                    'product_uom_qty': line_id.product_qty * self.product_qty,
+                    'product_uom': line_id.product_uom_id.id,
+                    'location_id': source_location.id,
+                    'location_dest_id': line_id.product_id.property_stock_production.id,
+                    'raw_material_production_id': self.id,
+                    'company_id': self.company_id.id,
+                    'price_unit': line_id.product_id.standard_price,
+                    'procure_method': 'make_to_stock',
+                    'origin': self.name,
+                    'warehouse_id': source_location.get_warehouse().id,
+                    'group_id': self.procurement_group_id.id,
+                    'propagate': self.propagate,
+                    'suggest_qty': line_id.product_qty * self.product_qty,
+                }
+                moves.create(data)
+
+        else:
+            for bom_line, line_data in exploded_lines:
+                moves += self._generate_raw_move(bom_line, line_data)
+        return moves
