f03f191fbed6dc76f470e5d29cbe79e7d463a8af
Allen <iverson2937@163.com>
2017-04-07 19:05:57 +0800

    fixbug

--- linkloving_warehouse/models/mrp_production.py
+++ linkloving_warehouse/models/mrp_production.py
@@ -6,4 +6,4 @@ from odoo.exceptions import UserError
 class MrpProduction(models.Model):
     _inherit = "mrp.production"
     source_mo_id = fields.Many2one('mrp.production')
-    source_mo_ids = fields.One2many('mrp.production', 'mo_id')
+    source_mo_ids = fields.One2many('mrp.production', 'source_mo_id')
