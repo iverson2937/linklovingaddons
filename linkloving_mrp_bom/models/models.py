845ca28d7c7c40d91555c5d13b2da2a4c2f7987e
allen <iverson2937@163.com>
2017-04-12 21:49:02 +0800

    添加状态

--- linkloving_mrp_bom/models/models.py
+++ linkloving_mrp_bom/models/models.py
@@ -9,9 +9,16 @@ class MrpBom(models.Model):
     state = fields.Selection([
         ('draft', u'草稿'),
         ('post', u'提交'),
+        ('document', u'文控审核'),
+        ('engineer', u'工程审核'),
+        ('ｍarket', u'市场'),
         ('done', u'正式')
     ], string=u'状态', default='draft')
 
     @api.multi
     def action_post(self):
-        self.state = 'done'
+        self.state = 'document'
+
+    @api.multi
+    def action_document(self):
+        self.state = 'document'
