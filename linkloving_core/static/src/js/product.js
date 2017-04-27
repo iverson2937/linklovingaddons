09249fc3cdc070c5bffeb31e5c0f8253635cf173
wangkefu <745376631@qq.com>
2017-04-27 11:31:07 +0800

    解决小三角形的bug，加了新展示的工艺等内容

--- linkloving_core/static/src/js/product.js
+++ linkloving_core/static/src/js/product.js
@@ -14,6 +14,7 @@ odoo.define('linkloving_core.product_detail', function (require) {
         events: {
             'click .click_tag_a': 'show_bom_line',
         },
+
         init: function (parent, action) {
             this._super.apply(this, arguments);
             this.product_id = action.product_id;
@@ -23,6 +24,9 @@ odoo.define('linkloving_core.product_detail', function (require) {
             var e = e || window.event;
             var target = e.target || e.srcElement;
             //小三角的变化
+            if(target.classList.contains('show_bom_line_process')||target.classList.contains('show_bom_line_service')){
+                target = target.parentNode;
+            }
             if(target.childNodes.length > 1){
                 if(target.childNodes[1].classList.contains("fa-caret-right")){
                     target.childNodes[1].classList.remove("fa-caret-right")
@@ -32,35 +36,31 @@ odoo.define('linkloving_core.product_detail', function (require) {
                     target.childNodes[1].classList.add("fa-caret-right")
                 }
             }
-
-
-            // if (target.classList.contains("fa-caret-right")) {
-            //     target.classList.remove("fa-caret-right");
-            //     target.classList.add("fa-caret-down");
-            //     // console.log(target.parentNode.getAttribute("data-id"));
-            //     //获取data-id
-            //     var data_id = target.parentNode.getAttribute("data-id");
-            //     //点击处所在的tr在tbody中的索引
-            //     var data_index = target.parentNode.sectionRowIndex;
-            //
-            //     new Model("product.template")
-            //     .call("get_detail", [48204])
-            //     .then(function (result) {
-            //         // console.log(result.bom_lines);
-            //         if(result.bom_lines.length!=0){
-            //             self.$(".bodys tr:eq("+data_index+")").after(QWeb.render('show_bom_line_tr_add', {bom_lines: result.bom_lines,pid:result.product_id}));
-            //         }
-            //         self.$(".show_product_name").html(result.name)
-            //     });
-            //
-            //
-            //     $(".tr_level_top").show();
-            // } else if (target.classList.contains("fa-caret-down")) {
-            //     target.classList.remove("fa-caret-down");
-            //     target.classList.add("fa-caret-right");
-            //     $(".tr_level_top").hide();
-            // }
-
+            if(target.classList.contains('open-sign')){
+                if(target.classList.contains("fa-caret-right")){
+                    target.classList.remove("fa-caret-right")
+                    target.classList.add("fa-caret-down")
+                }else if(target.classList.contains("fa-caret-down")){
+                    target.classList.remove("fa-caret-down")
+                    target.classList.add("fa-caret-right")
+                }
+                target = target.parentNode;
+            }
+            if(target.attributes['data-level'] && target.attributes['data-level'].nodeValue=='true'){
+                if(target.attributes['data-product-id']){
+                     var product_id = target.attributes['data-product-id'].nodeValue;
+                        product_id = parseInt(product_id);
+                        // console.log(product_id);
+                        new Model("product.template")
+                            .call("get_detail", [product_id])
+                            .then(function (result) {
+                                console.log(result);
+                                // console.log(self.$("#"+product_id+">.panel-body").html());
+                                self.$("#"+product_id+">.panel-body").html(" ");
+                                self.$("#"+product_id+">.panel-body").append(QWeb.render('show_bom_line_tr_add', {bom_lines: result.bom_lines,result:result}));
+                            });
+                 }
+            }
         },
         start: function () {
             var self = this;
@@ -68,7 +68,7 @@ odoo.define('linkloving_core.product_detail', function (require) {
                 .call("get_detail", [this.product_id])
                 .then(function (result) {
                     console.log(result);
-                    self.$el.append(QWeb.render('show_bom_line_tr', {bom_lines: result.bom_lines,p_name:result.name}));
+                    self.$el.append(QWeb.render('show_bom_line_tr', {bom_lines: result.bom_lines,result:result}));
                 });
 
         },
