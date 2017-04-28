a21a042099532bcdef1b26fb02d2337c9c8fb733
Allen <iverson2937@163.com>
2017-04-28 20:03:43 +0800

    update 分组

--- linkloving_product_group/models/models.py
+++ linkloving_product_group/models/models.py
@@ -4,5 +4,14 @@ from odoo import models, fields, api
 
 
 class ProductTemplate(models.Model):
-    _name = 'product.template'
-    is_domestic = fields.Boolean()
+    _inherit = 'product.template'
+    mo_ids = fields.One2many('mrp.production', 'product_tmpl_id',
+                             domain=[('state', 'in', ['draft', 'confirmed', 'waiting_material'])])
+
+    @api.depends('mo_ids')
+    def _compute_mo_ids(self):
+        for product in self:
+            if product.mo_ids:
+                product.has_mo = True
+
+    has_mo = fields.Boolean(compute=_compute_mo_ids, store=True)
