de203797efb18f4b01684ef57017001b8333b920
wangkefu <745376631@qq.com>
2017-05-02 11:17:01 +0800

    'update'

--- linkloving_core/static/src/js/product.js
+++ linkloving_core/static/src/js/product.js
@@ -8,14 +8,35 @@ odoo.define('linkloving_core.product_detail', function (require) {
     var QWeb = core.qweb;
     var _t = core._t;
 
-
     var KioskConfirm = Widget.extend({
         template: "HomePage",
         events: {
             'click .click_tag_a': 'show_bom_line',
             'click .show_po_number':'to_po_page',
             'click .show_mo_number': 'to_mo_page',
+            'click .chk_all': 'check_all',
+            'click .send-po-btn':'get_po_id'
         },
+        check_all:function (e) {
+            var e = e || window.event;
+            var target = e.target || e.srcElement;
+            var chk_id = target.getAttribute("id");
+            var abc=$("input[name="+chk_id+"]");
+            abc.each(function () {
+                $(this).prop("checked",target.checked)
+            })
+        },
+        get_po_id:function () {
+            var abc=$("input[name=chk51826]");
+            abc.each(function () {
+                console.log($(this).prop("checked"));
+                if($(this).prop("checked")){
+                    console.log('xxx')
+                }
+            })
+        },
+        
+
         to_po_page:function (e) {
             var e = e || window.event;
             var target = e.target || e.srcElement;
@@ -49,8 +70,6 @@ odoo.define('linkloving_core.product_detail', function (require) {
                 views: [[false, 'form']],
                 res_id: act_id,
                 target:"new"
-                // employee_name: this.record.name.raw_value,
-                // employee_state: this.record.attendance_state.raw_value,
             };
             this.do_action(action);
         },
@@ -66,7 +85,6 @@ odoo.define('linkloving_core.product_detail', function (require) {
             //若点击的是
             if(target.classList.contains('show_bom_line_one')||target.classList.contains('show_bom_line_two')){
                 target = target.parentNode;
-                console.log('sss')
             }
             //小三角的变化
             if(target.childNodes.length > 1){
@@ -91,6 +109,7 @@ odoo.define('linkloving_core.product_detail', function (require) {
             // if(target.attributes['data-level'] && target.attributes['data-level'].nodeValue=='true'){
                 if(target.attributes['data-product-id']){
                      var product_id = target.attributes['data-product-id'].nodeValue;
+                     var transform_service="";
                         product_id = parseInt(product_id);
                         // console.log(product_id);
                         new Model("product.template")
@@ -105,18 +124,39 @@ odoo.define('linkloving_core.product_detail', function (require) {
                                     for(var i=0;i<result.po_lines.length;i++){
                                         result.po_lines[i].date_planned = result.po_lines[i].date_planned.substr(0,10);
                                     }
-                                    // self.$("#"+product_id+">.panel-body").prepend(QWeb.render('show_bom_line_po',{po:result.po_lines}));
                                 }
                                 if(result.mo_ids.length>0){
                                     for(var i=0;i<result.mo_ids.length;i++){
                                         result.mo_ids[i].date = result.mo_ids[i].date.substr(0,10);
                                     }
-                                    // self.$("#"+product_id+">.panel-body").prepend(QWeb.render('show_bom_line_mo',{mo:result.mo_ids}));
                                 }
-                                // if(result.bom_lines.length>0){
-                                    self.$("#"+product_id+">.panel-body").append(QWeb.render('show_bom_line_tr_add', {bom_lines: result.bom_lines,result:result,po_length:po_length,bom_length:bom_length,mo_length: mo_length}));
-                                // }
 
+                                if(result.service){
+                                     for(var i=0;i<result.service.length;i++){
+                                        if(result.service[i]==1){
+                                            result.transform_service = transform_service + "订单制"
+                                        }else if(result.service[i]==5){
+                                            result.transform_service = transform_service + "制造制"
+                                        }else if(result.service[i]==6){
+                                            result.transform_service = transform_service + "采购"
+                                        }
+                                    }
+                                }
+                                for(var i=0;i<result.bom_lines.length;i++){
+                                    result.bom_lines[i].transform_service = "";
+                                    if(result.bom_lines[i].service){
+                                        for(var j=0;j<result.bom_lines[i].service.length;j++){
+                                            if(result.bom_lines[i].service[j]==1){
+                                                result.bom_lines[i].transform_service = result.bom_lines[i].transform_service + "订单制"
+                                            }else if(result.bom_lines[i].service[j]==5){
+                                                result.bom_lines[i].transform_service = result.bom_lines[i].transform_service + "制造制"
+                                            }else if(result.bom_lines[i].service[j]==6){
+                                                result.bom_lines[i].transform_service = result.bom_lines[i].transform_service + "采购"
+                                            }
+                                        }
+                                    }
+                                }
+                                self.$("#"+product_id+">.panel-body").append(QWeb.render('show_bom_line_tr_add', {bom_lines: result.bom_lines,result:result,po_length:po_length,bom_length:bom_length,mo_length: mo_length,transform_service:transform_service}));
 
                             });
                  }
@@ -129,23 +169,48 @@ odoo.define('linkloving_core.product_detail', function (require) {
                 .call("get_detail", [this.product_id])
                 .then(function (result) {
                     console.log(result);
+                    var transform_service="";
                     var po_length = result.po_lines.length;
                     var bom_length = result.bom_lines.length;
                     var mo_length = result.mo_ids.length;
-                    console.log(mo_length)
-                    self.$el.append(QWeb.render('show_bom_line_tr', {bom_lines: result.bom_lines,result:result,po_length:po_length,bom_length:bom_length,mo_length: mo_length}));
+                    //时间截取
                     if(result.mo_ids.length>0){
                         for(var i=0;i<result.mo_ids.length;i++){
                             result.mo_ids[i].date = result.mo_ids[i].date.substr(0,10);
                         }
-                        // self.$('.panel-body').prepend(QWeb.render('show_bom_line_mo',{mo:result.mo_ids}));
                     }
                     if(result.po_lines.length>0){
                         for(var i=0;i<result.po_lines.length;i++){
                             result.po_lines[i].date_planned = result.po_lines[i].date_planned.substr(0,10);
                         }
-                        // self.$('.panel-body').prepend(QWeb.render('show_bom_line_po',{po:result.po_lines}));
                     }
+                    if(result.service){
+                         for(var i=0;i<result.service.length;i++){
+                            if(result.service[i]==1){
+                                result.transform_service = transform_service + "订单制"
+                            }else if(result.service[i]==5){
+                                result.transform_service = transform_service + "制造制"
+                            }else if(result.service[i]==6){
+                                result.transform_service = transform_service + "采购"
+                            }
+                        }
+                    }
+                    for(var i=0;i<result.bom_lines.length;i++){
+                        result.bom_lines[i].transform_service = "";
+                        if(result.bom_lines[i].service){
+                            for(var j=0;j<result.bom_lines[i].service.length;j++){
+                                if(result.bom_lines[i].service[j]==1){
+                                    result.bom_lines[i].transform_service = result.bom_lines[i].transform_service + "订单制"
+                                }else if(result.bom_lines[i].service[j]==5){
+                                    result.bom_lines[i].transform_service = result.bom_lines[i].transform_service + "制造制"
+                                }else if(result.bom_lines[i].service[j]==6){
+                                    result.bom_lines[i].transform_service = result.bom_lines[i].transform_service + "采购"
+                                }
+                            }
+                        }
+                    }
+
+                    self.$el.append(QWeb.render('show_bom_line_tr', {bom_lines: result.bom_lines,result:result,po_length:po_length,bom_length:bom_length,mo_length: mo_length,transform_service:transform_service}));
 
                 });
             }
