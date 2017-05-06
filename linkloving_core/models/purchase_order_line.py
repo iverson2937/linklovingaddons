92d6abd329ce76cb03d29c3af9c366ebe3419a96
Allen <iverson2937@163.com>
2017-05-06 12:08:48 +0800

    update

--- linkloving_core/models/purchase_order_line.py
+++ linkloving_core/models/purchase_order_line.py
@@ -4,7 +4,7 @@ import json
 from odoo import models, fields, api
 
 
-class SaleOrderLine(models.Model):
+class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
     @api.multi
@@ -21,6 +21,7 @@ class SaleOrderLine(models.Model):
         for line in self:
             origin = line.order_id.origin
             sources = []
+            res = []
             if origin:
                 mos = origin.split(',')
 
@@ -28,3 +29,24 @@ class SaleOrderLine(models.Model):
                     mo_ids = mo.split(':')
                     for mo_id in mo_ids:
                         sources.append(mo_id.strip())
+            for s in set(sources):
+                if s.startswith('mo'):
+                    mo_id = self.env['mrp.production'].search([('name', '=', s)])
+                    for move_line in mo_id.sim_stock_move_lines:
+                        if line.product_id == move_line.product_id:
+                            res.append({
+                                'name': mo_id.name,
+                                'product_qty': line.product_qty,
+                                'date_planned_start': mo_id.date_planned_start,
+                                'state': mo_id.state
+                            })
+                else:
+                    so_id = self.env['mrp.production'].search([('name', '=', s)])
+                    for order_line_id in so_id.order_line:
+                        if line.product_id == order_line_id.product_id:
+                            res.append({
+                                'partner_name': so_id.partner_id.name,
+                                'name': so_id.name,
+                                'product_qty': order_line_id.product_qty,
+                                'date': so_id.validity_date,
+                            })
