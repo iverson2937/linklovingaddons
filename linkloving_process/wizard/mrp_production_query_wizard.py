2990c16020a6e1ec294e5d11e2753e4f63bb51cb
Allen <iverson2937@163.com>
2017-05-09 16:24:52 +0800

    git update

--- linkloving_process/wizard/mrp_production_query_wizard.py
+++ linkloving_process/wizard/mrp_production_query_wizard.py
@@ -15,17 +15,19 @@ class MrpProductionWizard(models.TransientModel):
     def _get_process_id(self):
         return self.env['mrp.process'].browse(self._context.get('active_ids'))[0]
 
-    process_id = fields.Many2one('mrp.process', default=_get_process_id)
+    process_id = fields.Many2many('mrp.process', default=_get_process_id)
 
     @api.multi
     def mo_query(self):
         start_date = datetime.datetime.strptime(self.start_date, "%Y-%m-%d").strftime('%Y-%m-%d %H:%M:%S')
         end_date = (datetime.datetime.strptime(self.end_date, "%Y-%m-%d") + datetime.timedelta(days=1)).strftime(
             '%Y-%m-%d %H:%M:%S')
+        if not self.process_id:
+            self.process_id = self.env['mrp.process'].search([])
         domain = [('date_planned_start', '>', start_date),
-                  ('date_planned_start', '<', end_date), ('process_id', '=', self.process_id.id),
-                  ('state', 'not in', ['cancel', 'draft', 'done'])]
-
+                  ('date_planned_start', '<', end_date), ('process_id', 'in', self.process_id.ids),
+                  ('state', 'not in', ['cancel', 'draft'])]
+        print domain
         return {
             'name': _('Manufacturing Order'),
             'res_model': 'mrp.production',
