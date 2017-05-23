d48ed5f710f8d3ec41150f0540473e28d92f61a8
Allen <iverson2937@163.com>
2017-05-23 15:09:20 +0800

    'update'

---
+++ linkloving_bom_update/models/res_partner.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+    customer_code=fields.Char(string=u'客户简称')
+
