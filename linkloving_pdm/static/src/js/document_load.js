0dd9fa1b5623c4c363893d8b1c005234f2d0fe03
wangkefu <745376631@qq.com>
2017-05-25 14:37:09 +0800

    pdm update

--- linkloving_pdm/static/src/js/document_load.js
+++ linkloving_pdm/static/src/js/document_load.js
@@ -32,22 +32,48 @@ odoo.define('linkloving_pdm.document_manage', function (require) {
                 target:"new"
             };
             this.do_action(action);
+            self.$(document).ajaxComplete(function ( event, xhr, settings) {
+                // "{"jsonrpc":"2.0","method":"call","params":{"model":"review.process.wizard","method":"search_read","args":[[["id","in",[10]]],["remark","partner_id","display_name","__last_update"]],"kwargs":{"context":{"lang":"zh_CN","tz":"Asia/Shanghai","uid":1,"default_product_attachment_info_id":"4","params":{},"bin_size":true,"active_test":false}}},"id":980816587}"
+                var data = JSON.parse(settings.data)
+                if(data.params.model == 'review.process.wizard'){
+                    if(data.params.method == 'action_to_next' ||
+                        data.params.method == 'action_pass' ||
+                        data.params.method == 'action_deny'
+                    ){
+                        var file_type = self.$("#document_tab").attr("data-now-tab");
+                        var product_id = parseInt(self.$("#document_tab").attr("data-product-id"));
+                        return new Model("product.template")
+                            .call("get_attachemnt_info_list", [product_id], {type:file_type})
+                            .then(function (result) {
+                                console.log(result);
+                                self.$("#"+file_type).html("");
+                                self.$("#"+file_type).append(QWeb.render('active_document_tab', {result: result}));
+                            })
+                    }
+                }
+            })
         },
         document_change_tabs:function (e) {
             var e = e||window.event;
             var target = e.target||e.srcElement;
             var file_type = $(target).attr("data");
+            self.$("#document_tab").attr("data-now-tab",file_type);
+            self.$("#document_tab").attr("data-product-id",this.product_id);
             return new Model("product.template")
                 .call("get_attachemnt_info_list", [this.product_id], {type:file_type})
                 .then(function (result) {
-                    console.log(result);
+                    console.log(result)
                     self.$("#"+file_type).html("");
                     self.$("#"+file_type).append(QWeb.render('active_document_tab', {result: result}));
                 })
         },
         init: function (parent, action) {
             this._super.apply(this, arguments);
-            this.product_id = action.product_id;
+            if(action.product_id) {
+                this.product_id = action.product_id;
+            }else {
+                this.product_id = action.params.active_id;
+            }
             var self = this;
         },
         start: function () {
