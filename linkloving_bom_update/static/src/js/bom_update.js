18747a17dffcb2f8d52baf3a2fb1ac32281ba3e6
wangkefu <745376631@qq.com>
2017-05-27 11:08:31 +0800

    pdm update

--- linkloving_bom_update/static/src/js/bom_update.js
+++ linkloving_bom_update/static/src/js/bom_update.js
@@ -213,9 +213,8 @@ odoo.define('linkloving_bom_update.bom_update', function (require) {
                 if (add_product_value != "") {
                     if(add_data_id_value != add_product_value){
                         json_data["if_input_changed"] = true;
-                        // add_product_value = add_product_value.replace(/^\[[0-9A-Za-z.]]$/)
-
-
+                        var reg = /\[[^\]]+]/i;
+                        add_product_value = add_product_value.replace(reg,"");
                         json_data["input_changed_value"] = add_product_value;
                     }else {
                         json_data["if_input_changed"] = false;
