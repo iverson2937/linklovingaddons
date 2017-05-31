9f30579df53c80b0c10796108f6a1120604ae251
Allen <iverson2937@163.com>
2017-05-31 20:24:59 +0800

    update bom

--- linkloving_bom_update/models/bom_update_wizard.py
+++ linkloving_bom_update/models/bom_update_wizard.py
@@ -270,14 +270,14 @@ class BomUpdateWizard(models.TransientModel):
 
     @staticmethod
     def get_new_product_name(old_name, postfix):
-        old = re.findall(ur"[^(<]+(?=[>）])", old_name)
+        old = re.findall(ur"[^({]+(?=[}）])", old_name)
         if old and len(old) == 1:
             new_name = old_name.replace(old[0], postfix)
         elif len(old) > 1:
             UserError(u'产品名称不规范，找不到想要的版本')
         else:
             if postfix:
-                new_name = old_name + '<' + postfix + '>'
+                new_name = old_name + '{' + postfix + '}'
             else:
                 new_name = old_name
         return new_name
