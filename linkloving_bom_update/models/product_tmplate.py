524bedf62aa97b0c3a2b56aa9b76d19f0528bbb8
Allen <iverson2937@163.com>
2017-06-02 16:08:58 +0800

    产品修改限制

--- linkloving_bom_update/models/product_tmplate.py
+++ linkloving_bom_update/models/product_tmplate.py
@@ -10,6 +10,24 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     @api.multi
+    def write(self, vals):
+        if 'RT-ENG' in vals['name'] and not self.env.user.has_group('mrp.group_mrp_manager'):
+            raise UserError(u'只有库存管理员才可以修改基础物料')
+        return super(ProductTemplate, self).write(vals)
+
+    @api.multi
+    def unlink(self):
+        if 'RT-ENG' in self.name and not self.env.user.has_group('mrp.group_mrp_manager'):
+            raise UserError(u'只有库存管理员才可以删除基础物料')
+        return super(ProductTemplate, self).unlink()
+
+    @api.model
+    def create(self, vals):
+        if 'RT-ENG' in vals['name'] and not self.env.user.has_group('mrp.group_mrp_manager'):
+            raise UserError(u'只有库存管理员才可以创建基础物料')
+        return super(ProductTemplate, self).create(vals)
+
+    @api.multi
     def bom_update(self):
         if not self.bom_ids:
             raise UserError(u'该产品没有BOM')
@@ -32,3 +50,21 @@ class ProductProduct(models.Model):
             'tag': 'bom_update',
             'bom_id': self.product_tmpl_id.bom_ids[0].id
         }
+
+    @api.multi
+    def write(self, vals):
+        if 'RT-ENG' in self.bom_id.product_tmpl_id.name and not self.env.user.has_group('mrp.group_mrp_manager'):
+            raise UserError(u'只有库存管理员才可以修改基础物料')
+        return super(ProductProduct, self).write(vals)
+
+    @api.multi
+    def unlink(self):
+        if 'RT-ENG' in self.name and not self.env.user.has_group('mrp.group_mrp_manager'):
+            raise UserError(u'只有库存管理员才可以删除基础物料')
+        return super(ProductProduct, self).unlink()
+
+    @api.model
+    def create(self, vals):
+        if 'RT-ENG' in vals['name'] and not self.env.user.has_group('mrp.group_mrp_manager'):
+            raise UserError(u'只有库存管理员才可以创建基础物料')
+        return super(ProductProduct, self).create(vals)
