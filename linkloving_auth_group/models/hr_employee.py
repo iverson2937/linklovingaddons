ed2a6945c0cb747bfe39313b48d85598f177cdf2
Allen <iverson2937@163.com>
2017-06-05 10:33:37 +0800

    用户名称不让重复

--- linkloving_auth_group/models/hr_employee.py
+++ linkloving_auth_group/models/hr_employee.py
@@ -17,20 +17,32 @@ class HrEmployee(models.Model):
 
     hr_job_ids = fields.Many2many('hr.job', 'hr_job_hr_employee_rel', 'job_id', 'employee_id')
 
+    # @api.multi
+    # @api.onchange('hr_job_ids')
+    # def _on_change_job_ids(self):
+    #     for emp in self:
+    #         if not emp.user_id:
+    #             raise UserError(u'请给员工设置相关用户')
+    #         groups = []
+    #         for job in emp.hr_job_ids:
+    #             for group in job.groups_id:
+    #                 groups.append(group.id)
+    #         print groups, 'dddddd'
+    #         self.env['res.users'].browse(emp.user_id.id).write({
+    #             'groups_id': [(6, 0, groups)]
+    #         })
     @api.multi
-    @api.onchange('hr_job_ids')
-    def _on_change_job_ids(self):
-        for emp in self:
-            if not emp.user_id:
-                raise UserError(u'请给员工设置相关用户')
-            groups = []
-            for job in emp.hr_job_ids:
-                for group in job.groups_id:
-                    groups.append(group.id)
-            print groups, 'dddddd'
-            self.env['res.users'].browse(emp.user_id.id).write({
-                'groups_id': [(6, 0, groups)]
-            })
+    def write(self, vals):
+        res = super(HrEmployee, self).write(vals)
+        groups = []
+        for job in self.hr_job_ids:
+            for group in job.groups_id:
+                groups.append(group.id)
+                self.env['res.users'].browse(self.user_id.id).write({
+                    'groups_id': [(6, 0, groups)]
+                })
+
+        return res
 
 # class ResUser(models.Model):
 #     _inherit = 'res.users'
