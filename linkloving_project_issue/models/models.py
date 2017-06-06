f5254d9956f600b3907b44507a9c897a522f5bc1
Allen <iverson2937@163.com>
2017-06-06 14:44:05 +0800

    update project

--- linkloving_project_issue/models/models.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-
-class ProjectIssue(models.Model):
-    _inherit = 'project.issue'
-    version_id = fields.Many2one('project.issue.version')
