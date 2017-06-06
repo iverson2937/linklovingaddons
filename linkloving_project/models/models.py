6901efd51260b76702f6d3e32d04aafbdb3b525e
Allen <iverson2937@163.com>
2017-06-06 11:50:57 +0800

    更新用户权限

--- linkloving_project/models/models.py
+++ linkloving_project/models/models.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api, _
 
 
 class linkloving_project(models.Model):
-    _inherit = 'project.project'
+    _name = 'project.project'
 
     state = fields.Selection([('template', 'Template'),
                               ('draft', 'New'),
