e9a714179c7bc26548f7248cfc3807050c66af1a
Allen <iverson2937@163.com>
2017-06-06 10:01:34 +0800

    update projects

--- linkloving_project_issue/models/models.py
+++ linkloving_project_issue/models/models.py
@@ -6,3 +6,4 @@ from odoo import models, fields, api
 class ProjectIssue(models.Model):
     _inherit = 'project.issue'
     version_id = fields.Many2one('project.issue.version')
+    categ_ids = fields.Many2many('project.category')
