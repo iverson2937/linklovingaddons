224bf35c109ecb303131b69fc18ae0d08cfc3fa8
Allen <iverson2937@163.com>
2017-06-06 11:25:06 +0800

    project issue

--- linkloving_project_issue/models/models.py
+++ linkloving_project_issue/models/models.py
@@ -11,3 +11,16 @@ class ProjectIssue(models.Model):
     project_escalation_id = fields.Many2one('project.project', 'Project Escalation',
                                             help='If any issue is escalated from the current Project, it will be listed under the project selected here.',
                                             states={'close': [('readonly', True)], 'cancelled': [('readonly', True)]})
+
+
+class ProjectProject(models.Model):
+    _inherit = 'project.project'
+
+    @api.depends('use_tasks', 'use_issues')
+    def on_change_use_tasks_or_issues(self):
+        values = {}
+        if self.use_tasks and not self.use_issues:
+            values['alias_model'] = 'project.task'
+        elif not self.use_tasks and self.use_issues:
+            values['alias_model'] = 'project.issue'
+        return {'value': values}
