0182fa853de673afeb610a361b2960292959eb26
Allen <iverson2937@163.com>
2017-06-06 09:44:36 +0800

    update project

--- linkloving_project_issue/models/models.py
+++ linkloving_project_issue/models/models.py
@@ -2,14 +2,7 @@
 
 from odoo import models, fields, api
 
-# class linkloving_project_issue(models.Model):
-#     _name = 'linkloving_project_issue.linkloving_project_issue'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+class ProjectIssue(models.Model):
+    _inherit = 'project.issue'
+    version_id = fields.Many2one('project.issue.version')
