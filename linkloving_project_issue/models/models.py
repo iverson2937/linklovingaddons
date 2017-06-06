50bd897e45a5d1c4ebb7144cf4daf340175bdd3f
Allen <iverson2937@163.com>
2017-06-06 10:11:23 +0800

    project

--- linkloving_project_issue/models/models.py
+++ linkloving_project_issue/models/models.py
@@ -8,3 +8,6 @@ class ProjectIssue(models.Model):
     version_id = fields.Many2one('project.issue.version')
     categ_ids = fields.Many2many('project.category')
     message_summary = fields.Text()
+    project_escalation_id = fields.Many2one('project.project', 'Project Escalation',
+                                            help='If any issue is escalated from the current Project, it will be listed under the project selected here.',
+                                            states={'close': [('readonly', True)], 'cancelled': [('readonly', True)]})
