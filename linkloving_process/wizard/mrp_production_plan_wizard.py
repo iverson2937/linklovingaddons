abc13cfa3185d66a6aa95de114f25254f2c5be97
Allen <iverson2937@163.com>
2017-06-07 10:24:29 +0800

    备料完成不允许修改mo状态

--- linkloving_process/wizard/mrp_production_plan_wizard.py
+++ linkloving_process/wizard/mrp_production_plan_wizard.py
@@ -18,6 +18,8 @@ class MrpProductionWizard(models.TransientModel):
         context = dict(self._context or {})
         active_ids = context.get('active_ids', []) or []
         for record in self.env['mrp.production'].browse(active_ids):
+            if record.state not in ['draft', 'confirmed', 'waiting_material']:
+                raise UserError((u"%s 已经 %s ,不能重新安排生产日期") % (record.name, record.state))
             record.date_planned_start = self.date_planned_start
             if self.partner_id:
                 record.in_charge_id = self.partner_id
