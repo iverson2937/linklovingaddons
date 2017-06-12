00ce1895c12e312d9cd5f26c2c77df449b5d82a4
Allen <iverson2937@163.com>
2017-06-12 14:10:06 +0800

    bom review

--- linkloving_bom_update/models/review_process_wizard.py
+++ linkloving_bom_update/models/review_process_wizard.py
@@ -7,3 +7,17 @@ class ReviewProcessWizard(models.TransientModel):
     _inherit = 'review.process.wizard'
 
     bom_id = fields.Many2one("mrp.bom")
+
+    # 送审
+    def send_to_review(self):
+        to_last_review = self._context.get("to_last_review")  # 是否送往终审
+        if not self.product_attachment_info_id.review_id:  # 如果没审核过
+            self.product_attachment_info_id.action_send_to_review()
+
+        self.product_attachment_info_id.state = 'review_ing'  # 被拒之后 修改状态 wei 审核中
+        self.product_attachment_info_id.review_id.process_line_review_now.submit_to_next_reviewer(
+            to_last_review=to_last_review,
+            partner_id=self.partner_id,
+            remark=self.remark)
+
+        return True
