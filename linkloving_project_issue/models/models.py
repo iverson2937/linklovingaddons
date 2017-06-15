3fafbc801c2d9e888dbfd2d86a75987b3c6b14b3
jason.lu@linkloving.com <Nt5211314!@#>
2017-06-15 11:11:03 +0800

    'gantt'

--- linkloving_project_issue/models/models.py
+++ linkloving_project_issue/models/models.py
@@ -13,3 +13,11 @@ from odoo import models, fields, api
 #     @api.depends('value')
 #     def _value_pc(self):
 #         self.value2 = float(self.value) / 100
+
+class linkloving_project_issue_version(models.Model):
+    _name = "linkloving.project.issue.version"
+    _order = "name desc"
+
+    name = fields.Char('Version Number', required=True);
+    active = fields.Boolean('Active', required=False, default=1);
+
