302c5bd888ec5731a89b96424049632d08ca8dd8
Allen <iverson2937@163.com>
2017-06-16 15:23:52 +0800

    update new mrp ruban

--- linkloving_new_mrp/models/mrp_production.py
+++ linkloving_new_mrp/models/mrp_production.py
@@ -6,6 +6,15 @@ from odoo import models, fields, api
 class NewMrpProduction(models.Model):
     _inherit = 'mrp.production'
     is_multi_output = fields.Boolean(default=True)
-    output_product_ids = fields.Many2many('product.product')
+    output_product_ids = fields.Many2one('mrp.production.material', 'mo_id', domain=[('type', '=', 'output')])
+    input_product_ids = fields.Many2one('mrp.production.material', 'mo_id', domain=[('type', '=', 'input')])
 
-# class InputMaterialLine
+
+class MrpProductionMaterial(models.Model):
+    _name = 'mrp.production.material'
+    product_id = fields.Many2one('product.product', string=u'产品', required=True)
+    mo_id = fields.Many2one('mrp.production')
+    type = fields.Selection([
+        ('input', '输入'),
+        ('output', '输出')
+    ])
