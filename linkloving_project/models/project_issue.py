984b96ae568825bf083193b345b6f21c4ae1409d
Allen <iverson2937@163.com>
2017-06-16 10:29:47 +0800

    update project

--- linkloving_project/models/project_issue.py
+++ linkloving_project/models/project_issue.py
@@ -1,10 +1,23 @@
+# -*- coding: utf-8 -*-
+
 from odoo import models, fields, api, _
+
+AVAILABLE_PRIORITIES = [
+    ('0', 'badly'),
+    ('1', 'Low'),
+    ('2', 'Normal'),
+    ('3', 'High'),
+    ('4', 'Very High'),
+    ('5', 'top level'),
+]
 
 
 class project_issue(models.Model):
     _inherit = 'project.issue'
 
     version_id = fields.Many2one('project.issue.version', 'Version')
+    planed_level = fields.Selection(AVAILABLE_PRIORITIES, string=u'计划星级')
+    actual_level = fields.Selection(AVAILABLE_PRIORITIES, string=u'质量星级')
 
     @api.onchange("project_id")
     def on_change_project(self):
