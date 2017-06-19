52e0c427b0e7582a3f586cd61962539e8293a511
wangkefu <745376631@qq.com>
2017-06-19 17:14:02 +0800

    'update'

---
+++ linkloving_mrp_automatic_plan/static/src/js/statuslight.js
@@ -0,0 +1,26 @@
+/**
+ * Created by 123 on 2017/6/19.
+ */
+odoo.define('web.StatusLight', function (require) {
+    "use strict";
+    var core = require('web.core');
+
+    var ListView = core.view_registry.get('list');
+    var Column=ListView.Column;
+    var list_widget_registry = core.list_widget_registry;
+
+    var StatusLight = Column.extend({
+        _format: function (row_data, options) {
+            console.log(row_data);
+            return _.template(
+                '<span class="fa fa-circle" style="color:<%-value%>"></span>')({
+                    value: _.str.sprintf("%.0f", row_data[this.status_light] || 0) == 1 ? 'green' : _.str.sprintf("%.0f", row_data[this.id].value || 0) == 2 ? 'yellow' : 'red'
+                });
+        }
+    })
+
+    list_widget_registry
+    .add('field.statuslight',StatusLight)
+
+
+});
