6d84aff624bb450d6f01075e97ff1b62e608d1ec
Allen <iverson2937@163.com>
2017-06-22 14:09:23 +0800

    update NOTIFY

--- linkloving_core/static/src/js/notify.js
+++ linkloving_core/static/src/js/notify.js
@@ -2,13 +2,16 @@ odoo.define('linkloving_core.notify', function (require) {
     "use strict";
 
     var core = require('web.core');
+    var Dialog = require('web.Dialog');
     core.action_registry.add('action_notify', function (element, action) {
         var params = action.params;
         if (params) {
-            element.do_notify(params.title, params.text, params.sticky);
+            var dialog = new Dialog({
+                'title': params.title,
+                'subtitle': params.text
+            })
         }
         return {'type': 'ir.actions.act_window_close'};
     });
-
 
 });
