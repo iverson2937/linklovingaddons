d9a673e72c219032fda7fe2ca568cf6aa8f89aed
Allen <iverson2937@163.com>
2017-07-03 19:59:15 +0800

    update

--- linkloving_product/models/product_template.py
+++ linkloving_product/models/product_template.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 
 class ProductTemplate(models.Model):
@@ -16,5 +17,32 @@ class ProductTemplate(models.Model):
         sub_spec_id = self.sub_spec_id.code if self.sub_spec_id else 0
         spec_id = self.spec_id.code
         full_specs = sub_spec_id + spec_id
-        pre_fix = '.'.join([self.cate])
-        customer_code = self.partner_id.customer_code if self.partner_id else version
+
+        if self.partner_id:
+            version1 = self.partner_id.customer_code
+            prefix = '.'.join([categ_code, full_specs, version1])
+            products = self.env['product.template'].search([('default_code', 'ilike', prefix)])
+            if not products:
+                final_version = 'A'
+            else:
+                versions = []
+                for product in products:
+                    if len(product.default_code.split('.')) > 3:
+                        versions.append(product.default_code.split('.')[-1])
+                if not versions:
+                    final_version = 'B'
+                else:
+                    final_version = max(versions + 1)
+            return '.'.join([categ_code, full_specs, version1, final_version])
+
+        else:
+            prefix = '.'.join([categ_code, full_specs])
+            products = self.env['product.template'].search([('default_code', 'ilike', prefix)])
+            if not products:
+                version1 = 000
+            else:
+                versions = []
+                for product in products:
+                    versions.append(product.default_code.split('.')[-1])
+                version1 = max(versions + 1)
+            return '.'.join([categ_code, full_specs, version1])
