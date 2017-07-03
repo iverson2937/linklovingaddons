b52736f8d777b514f92f303af675277592e85374
Allen <iverson2937@163.com>
2017-07-03 12:16:00 +0800

    fix bug

--- linkloving_process/models/res_partner.py
+++ linkloving_process/models/res_partner.py
@@ -7,5 +7,4 @@ from odoo.exceptions import UserError
 class ResPartner(models.Model):
     _inherit = 'res.partner'
     process_id = fields.Many2one('mrp.process')
-
-
+    is_in_charge = fields.Boolean(string='Is In Charge')
