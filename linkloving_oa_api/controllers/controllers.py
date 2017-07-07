7acb6dabd3520aa0ef2a2354af8f69de58dd4b23
charyn bryant <357346640@qq.com>
2017-07-07 13:02:54 +0800

    111

---
+++ linkloving_oa_api/controllers/controllers.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+from odoo.http import request
+
+
+class LinklovingOaApi(http.Controller):
+    @http.route('/linkloving_oa_api/get_company_by_name/', auth='none', type='json')
+    def get_company_by_name(self, **kwargs):
+        request.session.db = request.jsonrequest["db"]
+        request.params["db"] = request.jsonrequest["db"]
+        name = request.jsonrequest.get("name")
+        if u"有限公司" in name:
+            name = name.replace(u"有限公司", "")
+        elif u'有限责任公司' in name:
+            name = name.replace(u"有限责任公司", "")
+        elif u'责任有限公司' in name:
+            name = name.replace(u"责任有限公司", "")
+        elif u'公司' in name:
+            name = name.replace(u"公司", "")
+
+        partners = request.env["res.partner"].sudo().search_read([("name", "ilike", name)], fields=["name"])
+        if partners:  # 如果有代表重复了
+            return partners
+
+    @http.route('/linkloving_oa_api/get_origin/', auth='none', type='json')
+    def get_origin(self, **kwargs):
+        sources = request.env["crm.lead.source"].sudo().search_read([], fields=['name'])
+        return (sources)
