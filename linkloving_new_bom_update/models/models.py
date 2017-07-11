b56a69cf13b03af03b4445c2b7e7d0ea5da31ce3
Allen <iverson2937@163.com>
2017-07-11 11:17:32 +0800

    update bom

--- linkloving_new_bom_update/models/models.py
+++ linkloving_new_bom_update/models/models.py
@@ -50,7 +50,7 @@ class MrpBom(models.Model):
             if level < 6:
                 level += 1
             for l in line.child_line_ids:
-                bom_line_ids.append(_get_rec(l, level))
+                bom_line_ids.append(_get_rec(l, level, line))
             if level > 0 and level < 6:
                 level -= 1
         bom_id = line.product_id.product_tmpl_id.bom_ids
@@ -77,14 +77,14 @@ class MrpBom(models.Model):
         return res
 
 
-def _get_rec(object, level, qty=1.0, uom=False):
+def _get_rec(object, level, parnet, qty=1.0, uom=False):
     for l in object:
         bom_line_ids = []
         if l.child_line_ids:
             if level < 6:
                 level += 1
             for line in l.child_line_ids:
-                bom_line_ids.append(_get_rec(line, level))
+                bom_line_ids.append(_get_rec(line, level, parnet))
             if level > 0 and level < 6:
                 level -= 1
         bom_id = l.product_id.product_tmpl_id.bom_ids
@@ -109,7 +109,7 @@ def _get_rec(object, level, qty=1.0, uom=False):
             'product_specs': l.product_id.product_specs,
             'is_highlight': l.is_highlight,
             'id': l.id,
-            'parent_id': parent_id,
+            'parent_id': parnet.id,
             'qty': l.product_qty,
             'process_id': process_id,
             'level': level,
