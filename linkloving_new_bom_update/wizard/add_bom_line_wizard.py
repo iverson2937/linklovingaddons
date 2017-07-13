7f93c7c446aba3dc3f63b20848c1a157a52a78e1
Allen <iverson2937@163.com>
2017-07-13 15:42:30 +0800

    update

--- linkloving_new_bom_update/wizard/add_bom_line_wizard.py
+++ linkloving_new_bom_update/wizard/add_bom_line_wizard.py
@@ -9,12 +9,29 @@ class AddBomLineWizard(models.TransientModel):
     _name = "add.bom.line.wizard"
     product_id = fields.Many2one('product.product', string='产品名称')
     name = fields.Char(string='新产品名称')
+    to_add = fields.Boolean(string='新建')
 
     qty = fields.Float()
-    product_specs = fields.Text(related='product_id.product_specs')
+
+    product_specs = fields.Text(string=u'规格')
 
     @api.multi
-    def action_post(self):
+    def action_add(self):
+        process_id = False
+        if self.product_id.product_tmpl_id.bom_ids:
+            process_id = self.product_id.product_tmpl_id.bom_ids.process_id.name
+
+        return {
+            'qty': self.qty,
+            'name': self.product_id.name_get(),
+            'process_id': process_id,
+            'new_name': self.name,
+            'product_tmpl_id': self.product_id.product_tmpl_id.id,
+            'product_spec': self.product_specs
+        }
+
+    @api.multi
+    def action_edit(self):
         process_id = False
         if self.product_id.product_tmpl_id.bom_ids:
             process_id = self.product_id.product_tmpl_id.bom_ids.process_id.name
