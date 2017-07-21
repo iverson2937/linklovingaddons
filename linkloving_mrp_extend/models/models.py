ac707958ab0ab4b02ea2a2c10dc11ff8fc3bc47b
peter <1045751270@qq.com>
2017-07-21 10:20:19 +0800

    更新制造入库

--- linkloving_mrp_extend/models/models.py
+++ linkloving_mrp_extend/models/models.py
@@ -812,26 +812,26 @@ class MrpProductionExtend(models.Model):
             # return_m.do_retrurn()
         return res
 
-        # def _generate_finished_moves(self):
-        #     move = self.env['stock.move'].create({
-        #         'name': self.name,
-        #         'date': self.date_planned_start,
-        #         'date_expected': self.date_planned_start,
-        #         'product_id': self.product_id.id,
-        #         'product_uom': self.product_uom_id.id,
-        #         'product_uom_qty': self.product_qty,
-        #         'location_id': self.product_id.property_stock_production.id,
-        #         'location_dest_id': self.location_dest_id.id,
-        #         'move_dest_id': self.procurement_ids and self.procurement_ids[0].move_dest_id.id or False,
-        #         'procurement_id': self.procurement_ids and self.procurement_ids[0].id or False,
-        #         'company_id': self.company_id.id,
-        #         'production_id': self.id,
-        #         'origin': self.name,
-        #         'group_id': self.procurement_group_id.id,
-        #         'move_order_type': 'null' if self.move_finished_ids else 'manufacturing_orders',
-        #     })
-        #     move.action_confirm()
-        #     return move
+    def _generate_finished_moves(self):
+        move = self.env['stock.move'].create({
+            'name': self.name,
+            'date': self.date_planned_start,
+            'date_expected': self.date_planned_start,
+            'product_id': self.product_id.id,
+            'product_uom': self.product_uom_id.id,
+            'product_uom_qty': self.product_qty,
+            'location_id': self.product_id.property_stock_production.id,
+            'location_dest_id': self.location_dest_id.id,
+            'move_dest_id': self.procurement_ids and self.procurement_ids[0].move_dest_id.id or False,
+            'procurement_id': self.procurement_ids and self.procurement_ids[0].id or False,
+            'company_id': self.company_id.id,
+            'production_id': self.id,
+            'origin': self.name,
+            'group_id': self.procurement_group_id.id,
+            'move_order_type': 'null' if self.move_finished_ids else 'manufacturing_orders',
+        })
+        move.action_confirm()
+        return move
 
 
 class ChangeProductionQty(models.TransientModel):
