71690785bd585a2292b07bdc6941fa54e6b2d8f1
Allen <iverson2937@163.com>
2017-08-04 15:10:23 +0800

    update

--- linkloving_new_mrp/models/mrp_product_rule.py
+++ linkloving_new_mrp/models/mrp_product_rule.py
@@ -33,6 +33,6 @@ class MrpProductRuleLine(models.Model):
 
     @api.multi
     def write(self, vals):
-        if self._context.get('no_update') and 'rule_id' in vals:
-            vals.pop('rule_id')
-        return super(MrpProductRuleLine, self).write(vals)
+        # if self._context.get('no_update') and 'rule_id' in vals:
+        #     vals.pop('rule_id')
+        pass
