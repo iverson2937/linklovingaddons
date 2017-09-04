dd0f16d16fe1349c20ec3024a3926f96199f7ef2
Allen <iverson2937@163.com>
2017-09-04 16:01:30 +0800

    update dashboard

---
+++ linkloving_sale_dashboard/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "linkloving_sale_dashboard",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'sale', 'crm', 'sales_team', 'linkloving_account'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/crm_dashboard.xml',
+        'views/template.xml'
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'qweb': ['static/src/xml/*.xml'],
+}
