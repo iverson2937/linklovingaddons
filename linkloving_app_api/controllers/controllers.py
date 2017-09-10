cc20cc13e3803956a650f63542d436275b74b26d
charyn bryant <357346640@qq.com>
2017-09-10 12:11:20 +0800

    1.debug diy

--- linkloving_app_api/controllers/controllers.py
+++ linkloving_app_api/controllers/controllers.py
@@ -2976,7 +2976,7 @@ class LinklovingAppApi(http.Controller):
                 for o_move in sim_stock_move.stock_moves:
                     if o_move.state != 'cancel':
                         need_qty += o_move.product_uom_qty
-                print("charlie0910--------%d,%d" % (total_qty, need_qty))
+                logging.warning("charlie0910--------%d,%d" % (total_qty, need_qty))
                 # if float_compare(need_qty, sim_stock_move.product_uom_qty, precision_rounding=rounding) < 0:
                 if float_compare(total_qty, sim_stock_move.product_uom_qty, precision_rounding=rounding) > 0:
 
@@ -2997,7 +2997,7 @@ class LinklovingAppApi(http.Controller):
                     qty_split = sim_stock_move.stock_moves[0].product_uom._compute_quantity(
                             split_qty_unuom,
                             sim_stock_move.stock_moves[0].product_id.uom_id)
-                    print("charlie0910-1-------%d,%d" % (split_qty_unuom, qty_split))
+                    logging.warning("charlie0910-1-------%d,%d" % (split_qty_unuom, qty_split))
                     # _logger.warning(u"charlie_0712_log_2:qty_split:%s,", str(qty_split))
                     split_move = sim_stock_move.stock_moves[0].copy(
                             default={'quantity_done': qty_split, 'product_uom_qty': qty_split,
@@ -3033,6 +3033,7 @@ class LinklovingAppApi(http.Controller):
                                 true_list.append(False)
                                 #     states = sim_stock_move.stock_moves.mapped("state")
                                 #     if states in ["cancel", "done"]:
+                        logging.warning("charlie0910-2-------%d,%d" % (move['quantity_ready'], all(true_list)))
                         if all(true_list):
                             split_move = sim_stock_move.stock_moves[0].copy(
                                     default={'quantity_done': move['quantity_ready'],
