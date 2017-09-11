35f11d34602c7a1e367bcfe01e19fb6c926cad96
Allen <iverson2937@163.com>
2017-09-11 15:21:29 +0800

    fix bgu

--- linkloving_new_bom_update/models/models.py
+++ linkloving_new_bom_update/models/models.py
@@ -81,7 +81,7 @@ class MrpBom(models.Model):
                         'partner_id': self.env.user.partner_id.id,
                         'review_id': self.review_id.id,
                         'remark': '%s----->%s' % (self.state, u'更新'),
-                        'state': 'review_success',
+                        'state': 'waiting_review',
                         'last_review_line_id': line_ids[-1].get('id') if line_ids else False,
                         'review_order_seq': max([line.review_order_seq for line in self.review_id.review_line_ids]) + 1
                     })
