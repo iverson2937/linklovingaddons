ddc8d8dee3f46bb141d8b2d79201e1d8395d5f48
Allen <iverson2937@163.com>
2017-09-13 11:19:51 +0800

    fix report bug

--- linkloving_account/report/report.py
+++ linkloving_account/report/report.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-from odoo import models,api,_
+import datetime
 
+from odoo import models, api, _
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT
 
 class ReportPaymentApplication(models.AbstractModel):
     _name = 'report.payment.application'
@@ -16,3 +18,42 @@ class ReportPaymentApplication(models.AbstractModel):
         }
         print self.env[report.model].browse(self.id)
         return report_obj.render('linkloving_account.report_payment_application', docargs)
+
+
+class ReportPartnerLedger(models.AbstractModel):
+    _inherit = 'report.account.report_partnerledger'
+
+    def _lines(self, data, partner):
+        full_account = []
+        query_get_data = self.env['account.move.line'].with_context(data['form'].get('used_context', {}))._query_get()
+        reconcile_clause = "" if data['form']['reconciled'] else ' AND "account_move_line".reconciled = false '
+        params = [partner.id, tuple(data['computed']['move_state']), tuple(data['computed']['account_ids'])] + \
+                 query_get_data[2]
+        query = """
+            SELECT "account_move_line".id, "account_move_line".date, j.code, acc.code as a_code, acc.name as a_name, "account_move_line".ref, m.name as move_name, "account_move_line".name, "account_move_line".debit, "account_move_line".credit, "account_move_line".amount_currency,"account_move_line".currency_id, c.symbol AS currency_code
+            FROM """ + query_get_data[0] + """
+            LEFT JOIN account_journal j ON ("account_move_line".journal_id = j.id)
+            LEFT JOIN account_account acc ON ("account_move_line".account_id = acc.id)
+            LEFT JOIN res_currency c ON ("account_move_line".currency_id=c.id)
+            LEFT JOIN account_move m ON (m.id="account_move_line".move_id)
+            WHERE "account_move_line".partner_id = %s
+                AND m.state IN %s
+                AND "account_move_line".account_id IN %s AND """ + query_get_data[1] + reconcile_clause + """
+                ORDER BY "account_move_line".date"""
+        self.env.cr.execute(query, tuple(params))
+        res = self.env.cr.dictfetchall()
+        sum = 0.0
+        lang_code = self.env.context.get('lang') or 'en_US'
+        lang = self.env['res.lang']
+        lang_id = lang._lang_get(lang_code)
+        date_format = '%m/%d/%Y'
+        for r in res:
+            r['date'] = datetime.strptime(r['date'], DEFAULT_SERVER_DATE_FORMAT).strftime(date_format)
+            r['displayed_name'] = '-'.join(
+                r[field_name] for field_name in ('move_name', 'ref', 'name')
+                if r[field_name] not in (None, '', '/')
+            )
+            sum += r['debit'] - r['credit']
+            r['progress'] = sum
+            full_account.append(r)
+        return full_account
