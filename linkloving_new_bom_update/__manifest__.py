d0499d4031a806366822149c097f8180e22d6c15
peter <1045751270@qq.com>
2017-09-14 17:41:32 +0800

    更新 导出Excel

--- linkloving_new_bom_update/__manifest__.py
+++ linkloving_new_bom_update/__manifest__.py
@@ -24,7 +24,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
         'wizard/add_bom_line_wizard.xml',
