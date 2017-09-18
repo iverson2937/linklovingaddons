36df077555a4ef41a99cdbeffff9e7ee2a4895f8
Allen <iverson2937@163.com>
2017-09-18 11:36:52 +0800

    update for stock adjust

--- linkloving_stock_adjust/models/models.py
+++ linkloving_stock_adjust/models/models.py
@@ -1,69 +1,78 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 from odoo.exceptions import UserError
 
 
 class StockTransfer(models.Model):
     _name = 'stock.transfer'
     name = fields.Char('名称')
-    picking_type_id = fields.Many2one('')
-
-    input_product_ids = fields.Many2one('input.product.line', domain=[('product_type', '=', 'input')])
-    output_product_ids = fields.One2many('input.product.line', domain=[('product_type', '=', 'output')])
-
+    picking_type_id = fields.Many2one('stock.picking')
+    input_product_ids = fields.One2many('stock.transfer.line', 'transfer_id', domain=[('product_type', '=', 'input')])
+    output_product_ids = fields.One2many('stock.transfer.line', 'transfer_id', domain=[('product_type', '=', 'output')])
     @api.model
-    def _get_default_picking_type(self):
-        return self.env['stock.picking.type'].search([
-            ('code', '=', 'mrp_operation'),
-            (
-                'warehouse_id.company_id', 'in',
-                [self.env.context.get('company_id', self.env.user.company_id.id), False])],
-            limit=1).id
+    def _default_location_id(self):
+        company_user = self.env.user.company_id
+        warehouse = self.env['stock.warehouse'].search([('company_id', '=', company_user.id)], limit=1)
+        if warehouse:
+            return warehouse.lot_stock_id.id
+        else:
+            raise UserError(_('You must define a warehouse for the company: %s.') % (company_user.name,))
 
-    picking_type_id = fields.Many2one(
-        'stock.picking.type', 'Picking Type',
-        default=_get_default_picking_type, required=True)
+    location_id = fields.Many2one('stock.location', default=_default_location_id)
+
+    state = fields.Selection([
+        ('draft', u'草稿'),
+        ('confirm', u'确定'),
+        ('done', u'完成'),
+    ], default='draft')
+
+    # @api.model
+    # def _get_default_picking_type(self):
+    #     return self.env['stock.picking.type'].search([
+    #         ('code', '=', 'mrp_operation'),
+    #         (
+    #             'warehouse_id.company_id', 'in',
+    #             [self.env.context.get('company_id', self.env.user.company_id.id), False])],
+    #         limit=1).id
+    #
+    # picking_type_id = fields.Many2one(
+    #     'stock.picking.type', 'Picking Type',
+    #     default=_get_default_picking_type, required=True)
 
     @api.multi
     def confirm(self):
-        pass
+        self.state = 'confirm'
 
     @api.multi
-    def transfer(self):
-        pass
-
-    def _generate_raw_moves(self, exploded_lines):
-        self.ensure_one()
-        moves = self.env['stock.move']
-        source_location = self.picking_type_id.default_location_src_id
-
-        if not self.input_product_ids or not self.output_product_ids:
-            raise UserError('请添加投入产出')
-            input_product_ids = self.input_product_ids
-
-        for line_id in input_product_ids:
-            data = {
-                'name': self.name,
-                'date': self.date_planned_start,
-                'product_id': line_id.product_id.id,
-                'product_uom_qty': self.product_qty,
-                'product_uom': line_id.product_id.uom_id.id,
-                'location_id': source_location.id,
-                'location_dest_id': line_id.product_id.property_stock_production.id,
-                'raw_material_production_id': self.id,
-                'company_id': self.company_id.id,
-                'price_unit': line_id.product_id.standard_price,
-                'procure_method': 'make_to_stock',
-                'origin': self.name,
-                'warehouse_id': source_location.get_warehouse().id,
-                'propagate': self.propagate,
-            }
-            moves.create(data)
+    def confirm_transfer(self):
+        inv_id = self.env['stock.inventory'].create({
+            'filter': 'partial',
+            'name': 'abc'
+        })
+        inv_id.prepare_inventory()
+        print inv_id
+        for record in self.input_product_ids:
+            self.env['stock.inventory.line'].create({
+                'product_id': record.product_id.id,
+                'product_qty': record.product_id.qty_available - record.product_qty,
+                'inventory_id': inv_id.id,
+                'location_id': inv_id.location_id.id
+            })
+        for output in self.output_product_ids:
+            self.env['stock.inventory.line'].create({
+                'product_id': output.product_id.id,
+                'product_qty': output.product_id.qty_available + output.product_qty,
+                'inventory_id': inv_id.id,
+                'location_id': inv_id.location_id.id
+            })
+        inv_id.action_done()
 
 
 class StockTransferLine(models.Model):
+    _name = 'stock.transfer.line'
     transfer_id = fields.Many2one('stock.transfer', on_delete="cascade")
+    qty_available = fields.Float(related='product_id.qty_available')
     product_id = fields.Many2one('product.product')
     product_qty = fields.Float(string=u'数量')
     product_type = fields.Selection([
