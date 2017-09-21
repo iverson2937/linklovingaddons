013b8d9b24601a046fb67c26e0806e457874fc99
charyn bryant <357346640@qq.com>
2017-09-21 13:26:53 +0800

    批量设置mobom的工序

--- linkloving_develop_tools/models/models.py
+++ linkloving_develop_tools/models/models.py
@@ -275,6 +275,36 @@ class CreateOrderPointWizard(models.TransientModel):
             }
         }
 
+    def mo_to_bz_process(self):
+        mos = self.env["mrp.production"].search([('process_id', '=', False)])
+        mos.write({
+            'process_id': 26,
+        })
+        return {
+            "type": "ir.actions.client",
+            "tag": "action_notify",
+            "params": {
+                "title": str(len(mos)) + u"完成",
+                "text": u"完成",
+                "sticky": False
+            }
+        }
+
+    def bom_to_bz_process(self):
+        mos = self.env["mrp.bom"].search([('process_id', '=', False)])
+        mos.write({
+            'process_id': 26,
+        })
+        return {
+            "type": "ir.actions.client",
+            "tag": "action_notify",
+            "params": {
+                "title": str(len(mos)) + u"完成",
+                "text": u"完成",
+                "sticky": False
+            }
+        }
+
     def get_today_time_and_tz(self):
         if self.env.user.tz:
             timez = fields.datetime.now(pytz.timezone(self.env.user.tz)).tzinfo._utcoffset
