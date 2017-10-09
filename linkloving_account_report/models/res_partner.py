085ed672120274ff339076fba9061b365fdeb2f3
Allen <iverson2937@163.com>
2017-10-09 18:18:57 +0800

    update for  account

--- linkloving_account_report/models/res_partner.py
+++ linkloving_account_report/models/res_partner.py
@@ -10,7 +10,7 @@ class ResPartner(models.Model):
         for res in self:
             orders = res.sale_order_ids.filtered(lambda x: x.invoice_status == 'to invoice')
             res.receivable_amount = sum(order.shipped_amount for order in orders)
-            res.receivable_amount_real = res.credit - res.receivable_amount
+            res.receivable_amount_real = res.credit + res.receivable_amount
 
 
     receivable_amount = fields.Float(string=u'已出货未对账金额', compute=_compute_receivable_amount)
