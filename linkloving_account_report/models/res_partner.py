dd40ca496ad26147523830040cf0f7d821b67f09
Allen <iverson2937@163.com>
2017-10-09 16:38:57 +0800

    update for 应收账款

---
+++ linkloving_account_report/models/res_partner.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    def _compute_receivable_amount(self):
+        for res in self:
+            orders = res.sale_order_ids.filtered(lambda x: x.invoice_status == 'to invoice')
+            print orders
+            res.receivable_amount = sum(order.shipped_amount for order in orders)
+
+    receivable_amount = fields.Float(string=u'已出货未对账金额', compute=_compute_receivable_amount)
