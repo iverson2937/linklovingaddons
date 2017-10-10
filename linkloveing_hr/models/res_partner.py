946489941f61e4440ca240a80508606f996942a7
charyn bryant <357346640@qq.com>
2017-10-10 17:20:50 +0800

    1.各种奇葩需求

--- linkloveing_hr/models/res_partner.py
+++ linkloveing_hr/models/res_partner.py
@@ -6,11 +6,13 @@ from odoo.exceptions import UserError
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
-    @api.one
+
+    @api.multi
     @api.depends('employee_ids')
     def is_employee(self):
-        if self.employee_ids:
-            self.employee = True
+        for em in self:
+            if em.employee_ids:
+                em.employee = True
 
-    employee = fields.Boolean(compute='is_employee',store=True)
+    employee = fields.Boolean(compute='is_employee', store=True)
     employee_ids = fields.One2many('hr.employee', 'address_home_id')
