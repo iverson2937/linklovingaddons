d9c77dfc0a0716a977061b0d235c77da21076696
Allen <iverson2937@163.com>
2017-10-30 16:12:09 +0800

    for debug

--- linkloving_warehouse/models/stock_move.py
+++ linkloving_warehouse/models/stock_move.py
@@ -24,7 +24,7 @@
 ##############################################################################
 
 
-from odoo import fields, models,api,_
+from odoo import fields, models, api, _
 from odoo.exceptions import UserError
 
 
@@ -35,6 +35,10 @@ class StockMove(models.Model):
     def action_cancel(self):
         """ Cancels the moves and if all moves are cancelled it cancels the picking. """
         # TDE DUMB: why is cancel_procuremetn in ctx we do quite nothing ?? like not updating the move ??
+
+        for move in self:
+            if move.state == 'done':
+                print move.id, 'ddddddddddddddddddddddddddddddddddddddddddddd'
         if any(move.state == 'done' for move in self):
             raise UserError(_('You cannot cancel a stock move that has been set to \'Done\'.'))
 
@@ -60,4 +64,3 @@ class StockMove(models.Model):
         if procurements:
             procurements.check()
         return True
-
