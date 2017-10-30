ce14d684011eb9bb911cce022053dab549cd14aa
Allen <iverson2937@163.com>
2017-10-30 16:19:34 +0800

    debug

--- linkloving_warehouse/models/stock_move.py
+++ linkloving_warehouse/models/stock_move.py
@@ -1,31 +1,12 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright 2015 Eezee-It, MONK Software, Vauxoo
-#    Copyright 2013 Camptocamp
-#    Copyright 2009-2013 Akretion,
-#    Author: Emmanuel Samyn, Raphaël Valyi, Sébastien Beau,
-#            Benoît Guillot, Joel Grand-Guillaume, Leonardo Donelli
-#            Osval Reyes, Yanina Aular
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+
 
 
 from odoo import fields, models, api, _
 from odoo.exceptions import UserError
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class StockMove(models.Model):
@@ -38,6 +19,7 @@ class StockMove(models.Model):
 
         for move in self:
             if move.state == 'done':
+                _logger.warning("Could not find view object with view_id '%s'", move.id)
                 print move.id, 'ddddddddddddddddddddddddddddddddddddddddddddd'
         if any(move.state == 'done' for move in self):
             raise UserError(_('You cannot cancel a stock move that has been set to \'Done\'.'))
