d3a00dfd1c05ba62505fa37d7a090a64f707af27
Allen <iverson2937@163.com>
2017-10-31 18:01:13 +0800

    update for report

--- linkloveing_hr/models/hr_department.py
+++ linkloveing_hr/models/hr_department.py
@@ -8,3 +8,19 @@ class HrExpense(models.Model):
     _inherit = 'hr.department'
 
     allow_amount = fields.Float(string='允许最大金额')
+
+    def get_to_approve_id(self, employee_id, total_amount):
+        state = ''
+        if employee_id == self.manager_id:
+            # 报销金额小于部门允许金额直接通过
+            if not self.parent_id or (
+                        self.allow_amount and total_amount < self.allow_amount):
+                state = 'approve'
+                return state, False
+            else:
+                if not self.parent_id.manager_id:
+                    raise UserError(u'上级部门未设置审核人')
+        else:
+            if not self.manager_id:
+                raise UserError(u'请设置部门审核人')
+            return state, self.manager_id.user_id.id
