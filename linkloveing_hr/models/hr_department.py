7dfac3a6d16d69270ee956162dc336c6d4d7c7d6
Allen <iverson2937@163.com>
2017-11-01 09:28:45 +0800

    update hr

--- linkloveing_hr/models/hr_department.py
+++ linkloveing_hr/models/hr_department.py
@@ -11,16 +11,18 @@ class HrExpense(models.Model):
 
     def get_to_approve_id(self, employee_id, total_amount):
         state = ''
+        to_approve_id = False
         if employee_id == self.manager_id:
             # 报销金额小于部门允许金额直接通过
             if not self.parent_id or (
                         self.allow_amount and total_amount < self.allow_amount):
                 state = 'approve'
-                return state, False
             else:
                 if not self.parent_id.manager_id:
                     raise UserError(u'上级部门未设置审核人')
+                to_approve_id = self.parent_id.manager_id.user_id.id
         else:
             if not self.manager_id:
                 raise UserError(u'请设置部门审核人')
-            return state, self.manager_id.user_id.id
+            to_approve_id = self.manager_id.user_id.id
+        return state, to_approve_id
