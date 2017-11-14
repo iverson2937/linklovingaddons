e88fc3ef05673c2c2f22dd76c39dcf7fd9c1d351
Allen <iverson2937@163.com>
2017-11-14 09:09:57 +0800

    update account

--- linkloving_account/models/__init__.py
+++ linkloving_account/models/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
-from . import account_invoice, account_payment_register, res_partner_bank, product_category, res_partner, \
-    account_move_line, account_payment
+from . import account_invoice, account_payment, res_partner_bank, product_category, res_partner, \
+    account_move_line
