b1c32e247a047965c3a8fac37d6eeb8b64f05a11
peter <1045751270@qq.com>
2017-11-15 17:11:21 +0800

    登陆后发布文章

--- linkloving_web_blog/controllers/controllers.py
+++ linkloving_web_blog/controllers/controllers.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import json
+import odoo
 
 from odoo import http, _
 from odoo.http import request
@@ -10,13 +11,13 @@ from odoo import http, fields, _
 from odoo.addons.website.controllers.main import QueryURL
 from odoo.addons.website_blog.controllers.main import WebsiteBlog
 
-import base64
-
-
 # ------------------------------------------------------
 #         bolog_name = ['公告栏', '发布文章','my文章审核','原生审核']
 #         bolog_url = ['/blog/new_blog_index', '/blog/new_blog_create_index','/blog/ 主页id','/blog/ago/check']
 # ------------------------------------------------------
+from odoo.addons.web.controllers.main import ensure_db, Home
+
+
 class LinklovingWebBlog(http.Controller):
     @http.route('/blog/new_blog_index', type='http', auth='public', website=True, methods=['GET'], csrf=False)
     def new_blog_index_show(self, **kw):
@@ -39,7 +40,7 @@ class LinklovingWebBlog(http.Controller):
     def new_blog_create_index_show(self, **kw):
 
         if not request.env.user.active:
-            return http.local_redirect('/web/login')
+            return http.local_redirect('/web/login?redirect=blog.post')
 
         fields_one = http.request.env['blog.blog'].search([('is_all_blog', '=', False)])
         fields_two = http.request.env['blog.tag'].search([])
@@ -289,6 +290,38 @@ class LinklovingWebBlog(http.Controller):
                 # return bool(record.website_published)
 
         return {'data': 'ok'}
+
+
+class HomeLinkLoving(Home, http.Controller):
+    @http.route('/web/login', type='http', auth="none")
+    def web_login(self, redirect=None, **kw):
+        ensure_db()
+        request.params['login_success'] = False
+        if request.httprequest.method == 'GET' and redirect and request.session.uid:
+            return http.redirect_with_hash(redirect)
+
+        if not request.uid:
+            request.uid = odoo.SUPERUSER_ID
+
+        values = request.params.copy()
+        try:
+            values['databases'] = http.db_list()
+        except odoo.exceptions.AccessDenied:
+            values['databases'] = None
+
+        if request.httprequest.method == 'POST':
+            old_uid = request.uid
+            uid = request.session.authenticate(request.session.db, request.params['login'], request.params['password'])
+            if uid is not False:
+                request.params['login_success'] = True
+                if not redirect:
+                    redirect = '/web'
+                if redirect == 'blog.post':
+                    return http.local_redirect('/blog/new_blog_create_index')
+                return http.redirect_with_hash(redirect)
+            request.uid = old_uid
+            values['error'] = _("Wrong login/password")
+        return request.render('web.login', values)
 
 
 class WebsiteBlogLinkLoving(WebsiteBlog, http.Controller):
