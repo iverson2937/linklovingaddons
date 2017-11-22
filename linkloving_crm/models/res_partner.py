36909b7a2e6a1b941b4f9552229f625f0afbba6e
peter <1045751270@qq.com>
2017-11-22 13:33:49 +0800

    创建商机

--- linkloving_crm/models/res_partner.py
+++ linkloving_crm/models/res_partner.py
@@ -177,7 +177,7 @@ class ResPartner(models.Model):
                     #     vals['mutual_rule_id'] = vals.get('mutual_customer_id')
 
         res = super(ResPartner, self).create(vals)
-        if res.customer:
+        if (not (vals.get('company_type') == 'person')) and res.customer:
             lead_vals = {
 
                 'name': "默认商机-" + str(res.name),
