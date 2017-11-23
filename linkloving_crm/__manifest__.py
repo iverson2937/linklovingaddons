9748e71b9a7222bcfcf1426513b153ffdee18fe2
Allen <iverson2937@163.com>
2017-11-23 17:27:04 +0800

    update crm

--- linkloving_crm/__manifest__.py
+++ linkloving_crm/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'crm', 'web', 'sales_team', 'website_partner'],
+    'depends': ['base', 'crm', 'web', 'sales_team'],
 
     # always loaded
     'data': [
