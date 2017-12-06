00d69bca2bebadc5e76187589b81de7d1fcae42a
Allen <iverson2937@163.com>
2017-12-06 18:11:06 +0800

    update

--- linkloving_custom_auth/__manifest__.py
+++ linkloving_custom_auth/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'linkloiving_sale', 'linkloving_purchase', 'stock'],
+    'depends': ['base', 'linkloving_sale', 'linkloving_purchase', 'stock'],
 
     # always loaded
     'data': [
