5871a3f5f966f010a6dee45cab66f365cb13b23b
Allen <iverson2937@163.com>
2017-12-08 09:55:16 +0800

    update rule

--- linkloving_custom_rule/__manifest__.py
+++ linkloving_custom_rule/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'linkloving_mrp_extend'],
+    'depends': ['base', 'linkloving_mrp_extend', 'linkloving_purchase', 'linkloving_warehouse'],
 
     # always loaded
     'data': [
