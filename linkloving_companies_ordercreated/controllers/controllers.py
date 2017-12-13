c385710330c088561c261b89cd9e3c36dabcc16b
charyn bryant <357346640@qq.com>
2017-12-13 10:47:24 +0800

    1.采购单手动更新价格

--- linkloving_companies_ordercreated/controllers/controllers.py
+++ linkloving_companies_ordercreated/controllers/controllers.py
@@ -6,11 +6,44 @@ from odoo.http import request
 
 
 class LinklovingCompanies(http.Controller):
+    @http.route('/linkloving_web/precost_price', auth='none', type='json', csrf=False)
+    def precost_price(self, **kw):
+        db = request.jsonrequest.get("db")  # 所选账套
+        request.session.db = db  # 设置账套
+        request.params["db"] = db
+
+        vals = request.jsonrequest.get("vals")  # 需要查询的产品数据
+        discount_to_sub = request.jsonrequest.get("discount_to_sub")  # 折算率
+        data_return = []
+        for val in vals:
+            default_code = val["default_code"]
+            try:
+                p_obj = request.env["product.product"].sudo().search([("default_code", "=", default_code)])
+            except OperationalError:
+                return {
+                    "code": -2,
+                    "msg": u"账套%s不存在" % db
+                }
+            if not p_obj:
+                return {
+                    "code": -4,
+                    "msg": u"%s此料号在%s账套中找不到" % (default_code, db)
+                }
+            data_return.append((1, val["line_id"], {
+                'line_id': val["line_id"],
+                'price_unit': p_obj.pre_cost_cal() / discount_to_sub
+            }))
+
+        return {
+            "code": 1,
+            'order_line': data_return,
+        }
+
     @http.route('/linkloving_web/create_order', auth='none', type='json', csrf=False)
     def ll_call_kw(self, **kw):
         db = request.jsonrequest.get("db")  # 所选账套
         vals = request.jsonrequest.get("vals")  # so的数据
-        discount_to_sub = request.jsonrequest.get("discount_to_sub")  # 折算率
+        # discount_to_sub = request.jsonrequest.get("discount_to_sub")  # 折算率
         request.session.db = db  # 设置账套
         request.params["db"] = db
         try:  #获取下单公司信息
@@ -30,7 +63,7 @@ class LinklovingCompanies(http.Controller):
         try:#创建so
             order_line_vals = vals["order_line"]
             order_line = []
-            order_line_return = []  #往回传的line信息
+            # order_line_return = []  #往回传的line信息
             for line in order_line_vals:
                 default_code = line["default_code"]
                 p_obj = request.env["product.product"].sudo().search([("default_code", "=", default_code)])
@@ -39,17 +72,17 @@ class LinklovingCompanies(http.Controller):
                         "code": -4,
                         "msg": u"%s此料号在%s账套中找不到" % (default_code, db)
                     }
-                price_after_dis = p_obj.pre_cost_cal() / discount_to_sub
+                # price_after_dis = p_obj.pre_cost_cal() / discount_to_sub
                 one_line_val = {
                     'product_id': p_obj.id,
                     'product_uom': p_obj.uom_id.id,
                     'product_uom_qty': line["product_uom_qty"],
-                    'price_unit': price_after_dis,
+                    'price_unit': line["price_unit"],
                 }
-                order_line_return.append((1, line["line_id"], {
-                    # 'line_id': line["line_id"],
-                    'price_unit': price_after_dis
-                }))
+                # order_line_return.append((1, line["line_id"], {
+                #     # 'line_id': line["line_id"],
+                #     'price_unit': price_after_dis
+                # }))
                 order_line.append([0, False, one_line_val])
             vals["order_line"] = order_line
             so = request.env["sale.order"].sudo().create(vals)
@@ -57,10 +90,10 @@ class LinklovingCompanies(http.Controller):
                 "code": 1,
                 'so': so.name,
                 'so_id': so.id,
-                'order_line': order_line_return,
+                # 'order_line': order_line_return,
             }
         except Exception, e:
             return {
                 "code": -1,
-                "msg": u"创建订单出现异常,请联系管理员"
+                "msg": u"创建订单出现异常, %s" % e.name if hasattr(e, "name") else '',
             }
--- linkloving_companies_ordercreated/models/models.py
+++ linkloving_companies_ordercreated/models/models.py
@@ -22,28 +22,82 @@ class ResPartnerExtend(models.Model):
 class PurchaseOrderExtend(models.Model):
     _inherit = 'purchase.order'
 
+    def get_precost_price(self):
+        if self.partner_id.sub_company == 'sub':
+            response = self.request_to_get_price()
+            if response:
+                order_line = response.get("order_line")
+                self.write({
+                    'order_line': order_line
+                })
+            else:
+                raise UserError(u'未收到返回')
+        return {
+            "type": "ir.actions.client",
+            "tag": "action_notify",
+            "params": {
+                "title": u"成功",
+                "text": u"价格更新成功",
+                "sticky": False
+            }
+        }
     def button_confirm(self):
         # todo
         res = super(PurchaseOrderExtend, self).button_confirm()
         if self.partner_id.sub_company == 'sub':
             response = self.request_to_create_so()
-            order_line = response.get("order_line")
-            self.write({
-                'order_line': order_line
+            if response:
+                pass
+                # order_line = response.get("order_line")
+                # self.write({
+                #     'order_line': order_line
+                # })
+            else:
+                raise UserError(u'未收到返回')
+        return {
+            "type": "ir.actions.client",
+            "tag": "action_notify",
+            "params": {
+                "title": u"操作成功",
+                "text": u"自动创建销售单成功! 单号为:%s" % response.get("so"),
+                "sticky": False
+            }
+        }
+
+    def get_request_info(self, str):
+        host = self.partner_id.request_host
+        if not host.startswith("http://"):
+            host = "http://" + host
+        url = host + str
+        db = self.partner_id.db_name
+        header = {'Content-Type': 'application/json'}
+        return url, db, header
+
+    def request_to_get_price(self):
+        line_list = []
+        for order_line in self.order_line:
+            line_list.append({
+                "line_id": order_line.id,
+                "default_code": order_line.product_id.default_code,
             })
-        return res
+        url, db, header = self.get_request_info('/linkloving_web/precost_price')
+        response = requests.post(url, data=json.dumps({
+            "db": db,
+            "discount_to_sub": self.partner_id.discount_to_sub,
+            "vals": line_list,
+        }), headers=header)
+        res_json = json.loads(response.content).get("result")
+        if res_json and res_json.get("code") < 0:
+            raise UserError(res_json.get("msg"))
+        return res_json
 
     def request_to_create_so(self):
         so = self._prepare_so_values()  # 解析采购单,生成so单信息
         host = self.partner_id.request_host
-        if not host.startswith("http://"):
-            host = "http://" + host
-        url = host + '/linkloving_web/create_order'
-        db = self.partner_id.db_name
-        header = {'Content-Type': 'application/json'}
+        url, db, header = self.get_request_info('/linkloving_web/create_order')
         try:
             response = requests.post(url, data=json.dumps({
-                "discount_to_sub": self.partner_id.discount_to_sub,
+                # "discount_to_sub": self.partner_id.discount_to_sub,
                 "db": db,
                 "vals": so,
             }), headers=header)
@@ -74,6 +128,7 @@ class PurchaseOrderExtend(models.Model):
                 "default_code": order_line.product_id.default_code,
                 "product_name": order_line.product_id.name,
                 "product_uom_qty": order_line.product_qty,
+                "price_unit": order_line.price,
             })
         data["order_line"] = line_list
         return data
