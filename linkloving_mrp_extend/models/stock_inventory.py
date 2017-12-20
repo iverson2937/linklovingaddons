be6cf8dc7e2cad247b05d746f1653420b3a6a671
Allen <iverson2937@163.com>
2017-12-20 16:29:52 +0800

    fix bug

--- linkloving_mrp_extend/models/stock_inventory.py
+++ linkloving_mrp_extend/models/stock_inventory.py
@@ -31,8 +31,7 @@ class InventoryLine(models.Model):
                 continue
             diff = line.theoretical_qty - line.product_qty
             vals = {
-                'name': _('INV:') + (
-                line.remark_adjust if hasattr(line, 'remark_adjust') else (line.inventory_id.name or '')),
+                'name': _('INV:') + (line.inventory_id.name or ''),
                 'product_id': line.product_id.id,
                 'product_uom': line.product_uom_id.id,
                 'date': line.inventory_id.date,
