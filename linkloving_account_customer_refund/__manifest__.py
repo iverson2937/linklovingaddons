923f982784e348c98f9184bc443e948a241c8b75
allen <iverson2937@163.com>
2017-12-26 11:20:52 +0800

    update account

--- linkloving_account_customer_refund/__manifest__.py
+++ linkloving_account_customer_refund/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'account', 'sale'],
+    'depends': ['base', 'account', 'sales_team'],
 
     # always loaded
     'data': [
