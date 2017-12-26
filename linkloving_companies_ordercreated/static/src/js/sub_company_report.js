4663b8af4aa1838614e570c76e3569cad99d2f0e
charyn bryant <357346640@qq.com>
2017-12-26 18:17:37 +0800

    1.跟踪单报表

--- linkloving_companies_ordercreated/static/src/js/sub_company_report.js
+++ linkloving_companies_ordercreated/static/src/js/sub_company_report.js
@@ -22,7 +22,10 @@ odoo.define('linkloving_companies_ordercreated.sub_company_report', function (re
             }
             if (action && action.context && action.context["sub_company_order_track"]) {
                 this.sub_company_order_track = true;
-                this.so_id = action.context["active_id"];
+            }
+            if (action && action.params) {
+                this.so_id = action.params["active_id"];
+                this.sub_company_order_track = true;
             }
         },
 
