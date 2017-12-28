9e4500d523c2437e0175c3473acb6ef69dd23952
jason.lu@linkloving.com <Nt5211314!@#>
2017-12-28 09:45:37 +0800

    增加不同任务的状态

---
+++ linkloving_work_order/models/linkloving_work_order.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+WORK_ORDER_ISSUE_STATE_UNASSIGNED = 1
+WORK_ORDER_ISSUE_STATE_PROCESS = 2
+WORK_ORDER_ISSUE_STATE_CHECK = 3
+WORK_ORDER_ISSUE_STATE_FINISH = 9
+
+class linkloving_work_order(models.Model):
+    _name = 'linkloving.work.order'
+
+    order_number = fields.Char()
+
+    name = fields.Char()
+
+    execute_uid = fields.Many2one('res_user')
+
+    effective_department_ids = fields.Many2many('hr_department', 'linkloving_work_order_department_rel', 'work_order_id', 'department_id', 'Department id')
+
+    priority = fields.Integer()
+
+    description = fields.Text()
+
+    create_date = fields.Date()
+
+    issue_state = fields.Integer()
+
+    assign_time = fields.Datetime()
+
+    finish_time = fields.Datetime()
+
+    attachments = fields.One2many(comodel_name="linkloving.work.order.image", inverse_name="work_order_id", string="工单图片",
+                              required=False, )
