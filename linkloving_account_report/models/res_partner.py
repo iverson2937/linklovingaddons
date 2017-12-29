6cc836d402f97b181b24a22c4c7de42ebe6ed742
allen <iverson2937@163.com>
2017-12-29 16:33:16 +0800

    update

--- linkloving_account_report/models/res_partner.py
+++ linkloving_account_report/models/res_partner.py
@@ -16,11 +16,15 @@ class ResPartner(models.Model):
         for res in self:
             orders = self.env['purchase.order'].search(
                 [('partner_id', '=', res.id), ('invoice_status', '=', 'to invoice')])
-            res.payable_amount = sum(order.shipped_amount for order in orders) - sum(
-                order.invoiced_amount for order in orders)
+            res.payable_amount = sum(order.shipped_amount for order in orders)
             res.payable_amount_real = res.debit + res.payable_amount
 
     receivable_amount = fields.Float(string=u'已出货未对账金额', compute=_compute_receivable_amount)
     payable_amount = fields.Float(string=u'已收货未对账金额', compute=_compute_payable_amount)
     payable_amount_real = fields.Float(string=u'总计', compute=_compute_payable_amount)
     receivable_amount_real = fields.Float(string=u'总计', compute=_compute_receivable_amount)
+
+    def _compute_pre_payment_amount(self):
+        pass
+
+    pre_payment_amount = fields.Float(string=u'预付款金额', compute=_compute_pre_payment_amount)
