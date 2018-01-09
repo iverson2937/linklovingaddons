45ad56d66d5865ad17fd7ad6d1932b7cc187b8d9
jason.lu@linkloving.com <Nt5211314!@#>
2018-01-09 13:41:02 +0800

    增加不同任务的状态

--- linkloving_work_order/__manifest__.py
+++ linkloving_work_order/__manifest__.py
@@ -26,6 +26,7 @@
     'data': [
         'security/ir.model.access.csv',
         'views/calendar_views.xml',
+        'views/data.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
