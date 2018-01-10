a4c48320b3a6f2fcc2c55e42593b8bb04c0a19c0
allen <iverson2937@163.com>
2018-01-10 18:54:00 +0800

    update stock

--- linkloving_warehouse/models/stock_inventory.py
+++ linkloving_warehouse/models/stock_inventory.py
@@ -6,7 +6,7 @@ from odoo import api, fields, models
 
 class StockInventoryInherit(models.Model):
     _inherit = 'stock.inventory'
-    adjust_type = fields.Selection([
+    remark = fields.Selection([
         ('transfer', '物料转换'),
         ('adjust', '库存调整')
     ], string=u'来源备注', defalut='adjust')
