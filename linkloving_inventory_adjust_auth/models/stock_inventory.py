d901787cd86e6abd8660733be9ba61bc855b6642
allen <iverson2937@163.com>
2018-01-15 16:13:46 +0800

    update

--- linkloving_inventory_adjust_auth/models/stock_inventory.py
+++ linkloving_inventory_adjust_auth/models/stock_inventory.py
@@ -14,15 +14,15 @@ class StockInventoryInherit(models.Model):
     def _to_approve_id(self):
         for s in self:
             product_ll_types = s.mapped('line_ids.product_id.product_ll_type')
-            types = set(product_ll_types)
-            if len(types) > 1 and 'raw_material' in types:
+            types = list(set(product_ll_types))
+            if len(types) > 1 and 'raw material' in types:
                 raise UserError('原材料和制成品不可以同时盘点')
             elif len(types) == 1 and types[0] == 'raw_material':
                 s.to_approve_id = self.env['ir.values'].sudo().get_default(
-                    'stock.config.settings', 'raw_material_approve_id').id
+                    'stock.config.settings', 'raw_material_approve_id')
             else:
                 s.to_approve_id = self.env['ir.values'].sudo().get_default(
-                    'stock.config.settings', 'finished_material_approve_id').id
+                    'stock.config.settings', 'finished_material_approve_id')
 
     @api.multi
     def _compute_can_approve(self):
