abdada9f9539a0c2bd609ade29e20da6b7a65443
allen <iverson2937@163.com>
2018-01-23 14:30:09 +0800

    update for budget

---
+++ linkloving_account_budget/models/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from . import budget, hr_expense_sheet
---
+++ linkloving_account_budget/models/budget.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+from odoo.exceptions import UserError
+
+
+class AccountBudget(models.Model):
+    _name = 'linkloving.account.budget'
+    _order = 'create_date desc'
+    name = fields.Char()
+    department_id = fields.Many2one('hr.department', string=u'部门')
+    product_id = fields.Many2one('product.product', string=u'费用类别')
+    amount = fields.Float(string=u'预算总金额')
+    balance = fields.Float(string=u'预算余额', compute='get_budget_balance')
+    sheet_ids = fields.One2many('hr.expense.sheet', 'budget_id')
+    description = fields.Text(string=u'描述')
+    fiscal_year_id = fields.Many2one('account.fiscalyear')
+    state = fields.Selection([
+        ('draft', '草稿'),
+        ('done', '正式'),
+    ], default='draft')
+
+    _sql_constraints = [
+        ('name_company_uniq', 'unique(department_id, fiscal_year_id, product_id)', '每个部门费用类别只可以每年只可以做一次预算'),
+    ]
+
+    @api.multi
+    def unlink(self):
+        for budget in self:
+            if budget.state == 'formal':
+                raise UserError('不能删除')
+        return super(AccountBudget, self).unlink()
+
+    def set_to_formal(self):
+        self.state = 'done'
+
+    @api.multi
+    def get_budget_balance(self):
+        for budget in self:
+            budget.balance = budget.amount - sum(sheet.total_amount for sheet in budget.sheet_ids)
