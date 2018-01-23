90405e22d5ca50ec0449c28a52028c237dbaae52
charyn bryant <357346640@qq.com>
2018-01-23 18:21:49 +0800

    1.需要更新 warehouse, companies模块

--- linkloving_companies_ordercreated/__manifest__.py
+++ linkloving_companies_ordercreated/__manifest__.py
@@ -27,6 +27,7 @@
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/sub_company_eco.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
