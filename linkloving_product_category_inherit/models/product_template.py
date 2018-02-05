b66cd9a9c457a629e7d24a915d17c2dfbc37e3c8
allen <iverson2937@163.com>
2018-02-05 18:01:17 +0800

    fix bug

--- linkloving_product_category_inherit/models/product_template.py
+++ linkloving_product_category_inherit/models/product_template.py
@@ -3,10 +3,10 @@
 from odoo import models, fields, api
 
 
-class ProductCategory(models.Model):
+class ProductTemplate(models.Model):
     """
     产品分类
     """
-    _inherit = 'product.category'
+    _inherit = 'product.template'
     brand_id = fields.Many2one('product.category.brand', related='categ_id.brand_id')
     area_id = fields.Many2one('product.category.area', related='categ_id.area_id')
