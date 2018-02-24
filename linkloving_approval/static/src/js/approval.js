b45f0ebcb6a9816bf012116c68b6bd277a112500
peter <1045751270@qq.com>
2018-02-24 17:07:17 +0800

    产品文件上传 导航显示

--- linkloving_approval/static/src/js/approval.js
+++ linkloving_approval/static/src/js/approval.js
@@ -936,6 +936,9 @@ odoo.define('linkloving_approval.approval_core', function (require) {
                             self.$(".labels_body").html("");
                             self.$(".labels_body").append(tag_type_name_list);
                             own.render_pager(this);
+                            $('.o_breadcrumb_full').removeClass('o_hidden');
+                            $('.o_breadcrumb_full .o_cp_left').addClass('o_hidden');
+                            $('.o_breadcrumb_full .o_cp_right').addClass('o_hidden');
                         })
                 });
 
