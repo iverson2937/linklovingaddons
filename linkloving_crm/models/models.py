e42fcfdc33074e95f0b21029e0f60d4dc9d2037a
peter <1045751270@qq.com>
2018-02-28 16:47:14 +0800

    销售首页 更新

--- linkloving_crm/models/models.py
+++ linkloving_crm/models/models.py
@@ -352,11 +352,13 @@ class CrmLead(models.Model):
                 data_team_list.append(team_sale_sum)
 
         else:
-            all_salesman_team = self.env.user.sale_team_id.member_ids
+            # 根据用户获取销售团队 然后再除去团队管理者
+            all_salesman_team = self.env.user.sale_team_id.member_ids - self.env.user.sale_team_id.user_id
 
-            for new_salesman_one in all_salesman_team:
-                if self.env.ref('sales_team.group_sale_salesman_all_leads').id in new_salesman_one.groups_id.ids:
-                    all_salesman_team -= new_salesman_one
+            # 这是过滤销售团队里面 权限为所有文档的人
+            # for new_salesman_one in all_salesman_team:
+            #     if self.env.ref('sales_team.group_sale_salesman_all_leads').id in new_salesman_one.groups_id.ids:
+            #         all_salesman_team -= new_salesman_one
 
             if self.env.user.sale_team_id.user_id != self.env.user:
                 account_invoice_domain += [('date', '>=', year_begin), ('user_id', '=', self.env.uid)]
