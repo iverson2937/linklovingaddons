3d1d46546941a20cadae8b236a2f4441f18d964c
allen <iverson2937@163.com>
2018-03-02 00:23:47 +0800

    fix bug

--- linkloving_account/models/account_invoice.py
+++ linkloving_account/models/account_invoice.py
@@ -56,27 +56,3 @@ class AccountInvoice(models.Model):
             for balance_id in self.balance_ids:
                 amount += balance_id.amount
             self.remain_apply_balance = self.remain_apply_balance - amount
-
-
-class AcLine(models.Model):
-    _inherit = 'account.invoice.line'
-
-    @api.one
-    @api.depends('price_unit', 'discount', 'invoice_line_tax_ids', 'quantity',
-        'product_id', 'invoice_id.partner_id', 'invoice_id.currency_id', 'invoice_id.company_id',
-        'invoice_id.date_invoice')
-    def _compute_price(self):
-        currency = self.invoice_id and self.invoice_id.currency_id or None
-        price = self.price_unit
-        taxes = False
-        # print self.invoice_line_tax_ids
-        # if self.invoice_line_tax_ids:
-        #     print '-----------------------------------'
-        #     taxes = self.invoice_line_tax_ids.compute_all(price, currency, self.quantity, product=self.product_id, partner=self.invoice_id.partner_id)
-        #     print taxes
-        #     print taxes['total_excluded']
-        self.price_subtotal = price_subtotal_signed = -self.quantity * price
-        if self.invoice_id.currency_id and self.invoice_id.company_id and self.invoice_id.currency_id != self.invoice_id.company_id.currency_id:
-            price_subtotal_signed = self.invoice_id.currency_id.with_context(date=self.invoice_id.date_invoice).compute(price_subtotal_signed, self.invoice_id.company_id.currency_id)
-        sign = self.invoice_id.type in ['in_refund', 'out_refund'] and -1 or 1
-        self.price_subtotal_signed = price_subtotal_signed * sign
