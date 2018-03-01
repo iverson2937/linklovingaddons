7222876edd4b7e575c226796f756cc5e91166d20
allen <iverson2937@163.com>
2018-03-02 00:03:00 +0800

    fix bug

--- linkloving_account/models/account_invoice.py
+++ linkloving_account/models/account_invoice.py
@@ -75,7 +75,7 @@ class AcLine(models.Model):
         #     taxes = self.invoice_line_tax_ids.compute_all(price, currency, self.quantity, product=self.product_id, partner=self.invoice_id.partner_id)
         #     print taxes
         #     print taxes['total_excluded']
-        self.price_subtotal = price_subtotal_signed = taxes['total_excluded'] if taxes else self.quantity * price
+        self.price_subtotal = -self.quantity * price
         if self.invoice_id.currency_id and self.invoice_id.company_id and self.invoice_id.currency_id != self.invoice_id.company_id.currency_id:
             price_subtotal_signed = self.invoice_id.currency_id.with_context(date=self.invoice_id.date_invoice).compute(price_subtotal_signed, self.invoice_id.company_id.currency_id)
         sign = self.invoice_id.type in ['in_refund', 'out_refund'] and -1 or 1
