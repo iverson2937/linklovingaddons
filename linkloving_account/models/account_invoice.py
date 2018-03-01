7e203cb462bed130b50850ccdab741155dc38e0a
allen <iverson2937@163.com>
2018-03-01 23:56:50 +0800

    fix bug

--- linkloving_account/models/account_invoice.py
+++ linkloving_account/models/account_invoice.py
@@ -69,12 +69,12 @@ class AcLine(models.Model):
         currency = self.invoice_id and self.invoice_id.currency_id or None
         price = self.price_unit * (1 - (self.discount or 0.0) / 100.0)
         taxes = False
-        print self.invoice_line_tax_ids
-        if self.invoice_line_tax_ids:
-            print '-----------------------------------'
-            taxes = self.invoice_line_tax_ids.compute_all(price, currency, self.quantity, product=self.product_id, partner=self.invoice_id.partner_id)
-            print taxes
-            print taxes['total_excluded']
+        # print self.invoice_line_tax_ids
+        # if self.invoice_line_tax_ids:
+        #     print '-----------------------------------'
+        #     taxes = self.invoice_line_tax_ids.compute_all(price, currency, self.quantity, product=self.product_id, partner=self.invoice_id.partner_id)
+        #     print taxes
+        #     print taxes['total_excluded']
         self.price_subtotal = price_subtotal_signed = taxes['total_excluded'] if taxes else self.quantity * price
         if self.invoice_id.currency_id and self.invoice_id.company_id and self.invoice_id.currency_id != self.invoice_id.company_id.currency_id:
             price_subtotal_signed = self.invoice_id.currency_id.with_context(date=self.invoice_id.date_invoice).compute(price_subtotal_signed, self.invoice_id.company_id.currency_id)
