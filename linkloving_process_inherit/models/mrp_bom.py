7c6e82883590f713ec1d9627bbb20bc819573fdc
allen <iverson2937@163.com>
2018-03-06 15:14:28 +0800

    update 成本

--- linkloving_process_inherit/models/mrp_bom.py
+++ linkloving_process_inherit/models/mrp_bom.py
@@ -12,9 +12,11 @@ class MrpBom(models.Model):
         result = []
         # for line in self.bom_line_ids:
         #     res.append(self.get_bom_line(line))
-
+        if self.product_tmpl_id.product_ll_type:
+            product_type_dict = dict(
+                self.product_tmpl_id.fields_get(['product_ll_type'])['product_ll_type']['selection'])
         res = {
-            'id': 1,
+            'id': self.id,
             'pid': 0,
             'product_id': self.product_tmpl_id.id,
             'product_tmpl_id': self.product_tmpl_id.id,
@@ -22,33 +24,22 @@ class MrpBom(models.Model):
             'name': self.product_tmpl_id.name_get()[0][1],
             'code': self.product_tmpl_id.default_code,
             'process_id': [self.process_id.id, self.process_id.name],
-            # 'bom_ids': sorted(res, key=lambda product: product['code']),
-        }
-        res1 = {
-            'pid': 1,
-            'id': 2,
-            'product_id': self.product_tmpl_id.id,
-            'product_tmpl_id': self.product_tmpl_id.id,
-            'product_specs': self.product_tmpl_id.product_specs,
-            'name': self.product_tmpl_id.name_get()[0][1],
-            'code': self.product_tmpl_id.default_code,
-            'process_id': [self.process_id.id, self.process_id.name],
+            'product_type': product_type_dict[self.product_tmpl_id.product_ll_type]
             # 'bom_ids': sorted(res, key=lambda product: product['code']),
         }
         result.append(res)
-        result.append(res1)
+        if self.bom_line_ids:
+            line_ids = []
+            for line in self.bom_line_ids:
+                line_ids.append(self.get_bom_line(line, result, product_type_dict))
+        return result + sorted(line_ids, key=lambda product: product['has_lines'])
 
-        return result
-
-    def get_bom_line(self, line, level=0):
-        bom_line_ids = []
+    def get_bom_line(self, line, result, product_type_dict):
         if line.child_line_ids:
-            if level < 6:
-                level += 1
+
             for l in line.child_line_ids:
-                bom_line_ids.append(_get_rec(l, level, line))
-            if level > 0 and level < 6:
-                level -= 1
+                _get_rec(l, line, result, product_type_dict)
+
         bom_id = line.product_id.product_tmpl_id.bom_ids
         action = line.action_id
 
@@ -60,18 +51,17 @@ class MrpBom(models.Model):
 
         res = {
             'name': line.product_id.name_get()[0][1],
-            'product_type': line.product_id.product_ll_type,
+            'product_type': product_type_dict[line.product_id.product_ll_type],
             'product_id': line.product_id.id,
             'product_tmpl_id': line.product_id.product_tmpl_id.id,
             'id': line.id,
-            'parent_id': line.bom_id.id,
+            'has_lines': 0 if line.child_line_ids else 1,
+            'pid': line.bom_id.id,
             'product_specs': line.product_id.product_specs,
             'code': line.product_id.default_code,
             'qty': line.product_qty,
             'process_id': process_id,
-            'action_id': action_id,
-            'level': level,
-            'bom_ids': sorted(bom_line_ids, key=lambda product: product['code'])
+            'process_action': action_id,
         }
 
         return res
@@ -82,16 +72,12 @@ class MrpBom(models.Model):
             bom.cost = sum(line.cost for line in bom.bom_line_ids)
 
 
-def _get_rec(object, level, parnet, qty=1.0, uom=False):
+def _get_rec(object, parnet, result, product_type_dict):
     for l in object:
-        bom_line_ids = []
         if l.child_line_ids:
-            if level < 6:
-                level += 1
             for line in l.child_line_ids:
-                bom_line_ids.append(_get_rec(line, level, l))
-            if level > 0 and level < 6:
-                level -= 1
+                _get_rec(line, l, result, product_type_dict)
+
         bom_id = l.product_id.product_tmpl_id.bom_ids
         process_id = []
         if bom_id:
@@ -100,20 +86,22 @@ def _get_rec(object, level, parnet, qty=1.0, uom=False):
         res = {
             'name': l.product_id.name_get()[0][1],
             'product_id': l.product_id.id,
+            'product_type': product_type_dict[l.product_id.product_ll_type],
             'product_tmpl_id': l.product_id.product_tmpl_id.id,
             'code': l.product_id.default_code,
             'product_specs': l.product_id.product_specs,
             # 'is_highlight': l.is_highlight,
             # 'product_type': l.product_id.product_ll_type,
             'id': l.id,
+            'pid': parnet.id,
             'material_cost': '',
             'manpower_cost': '',
             'parent_id': parnet.id,
             'qty': l.product_qty,
             'process_id': process_id,
-            'level': level,
-            'bom_ids': bom_line_ids
+            # 'bom_ids': bom_line_ids
         }
+        result.append(res)
 
     return res
 
