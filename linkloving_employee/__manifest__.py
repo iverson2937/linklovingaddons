94341d5ca89b979dbed81fd4c052f2e639d95e96
allen <iverson2937@163.com>
2018-03-19 21:04:36 +0800

    update hr

--- linkloving_employee/__manifest__.py
+++ linkloving_employee/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'hr', 'hr_contract'],
+    'depends': ['base', 'hr', 'hr_contract', 'linkloving_mrp_extend'],
 
     # always loaded
     'data': [
