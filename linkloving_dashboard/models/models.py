6ce0b01751d8c33a5823e93c1164eae1a7f03151
allen <iverson2937@163.com>
2018-03-26 19:47:17 +0800

    update dashboard

--- linkloving_dashboard/models/models.py
+++ linkloving_dashboard/models/models.py
@@ -2,14 +2,17 @@
 
 from odoo import models, fields, api
 
-# class linkloving_dashboard(models.Model):
-#     _name = 'linkloving_dashboard.linkloving_dashboard'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+class Dashboard(models.TransientModel):
+    _name = 'linkloving.dashboard'
+
+    @api.model
+    def get_dashboard_data(self):
+        by_category = by_team = by_brand = []
+
+        return {
+            'by_category': by_category,
+            'by_team': by_team,
+            'by_brand': by_brand,
+
+        }
