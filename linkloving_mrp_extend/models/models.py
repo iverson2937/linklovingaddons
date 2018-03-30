6af76a58590ef276a1c8174302df0d2b30052363
charyn bryant <357346640@qq.com>
2018-03-30 13:25:18 +0800

    date 增加 format方法

--- linkloving_mrp_extend/models/models.py
+++ linkloving_mrp_extend/models/models.py
@@ -2094,7 +2094,7 @@ class StcokPickingExtend(models.Model):
                             move.state == "assigned" for move in pick.move_lines):
                 raise UserError(u"库存异动单异常,请联系管理员解决")
             if sum(pick.pack_operation_product_ids.mapped("qty_done")) == 0:
-                raise UserError(u"出货数量不能全部为0")
+                raise UserError(u"出货数量不能全部为0 %s" % pick.id)
         return super(StcokPickingExtend, self).to_stock()
 
     # 分拣完成
