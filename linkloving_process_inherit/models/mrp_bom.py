43dac0319e8af2f6cdbca2b8217bb6bb27f54b2f
allen <iverson2937@163.com>
2018-04-02 18:13:14 +0800

    update account

--- linkloving_process_inherit/models/mrp_bom.py
+++ linkloving_process_inherit/models/mrp_bom.py
@@ -441,11 +441,12 @@ class MrpBomLine(models.Model):
                         if action.get('id'):
                             process_action_line = action_line_obj.browse(action.get('id'))
                             action_id_new = int(action.get('action_id'))
-                            process_action_line.write({
-                                'rate': action.get('rate'),
-                                'rate_2': action.get('rate_2'),
-                                'action_id': action_id_new
-                            })
+                            if action_data in self.env['mrp.process.action'].search([]).ids:
+                                process_action_line.write({
+                                    'rate': action.get('rate'),
+                                    'rate_2': action.get('rate_2'),
+                                    'action_id': action_id_new
+                                })
                         elif action.get('delete_line_id'):
                             action_line_obj.browse(int(action.get('delete_line_id'))).unlink()
                         else:
