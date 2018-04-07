90316a8ed7bd089f7c30e3e518dc2f79e7afad98
wangkefu <745376631@qq.com>
2018-04-07 14:53:34 +0800

    update

--- linkloving_process_inherit/static/src/js/bom_cost_reproduce.js
+++ linkloving_process_inherit/static/src/js/bom_cost_reproduce.js
@@ -25,9 +25,10 @@ odoo.define('linkloving_process_inherit.bom_cost_reproduce', function (require) 
         confirm_target_bom_sel_func:function () {
             var origin_bom_id = $('.target_bom select option:selected').attr('data-bom-id');
             var target_bom_id = $('.target_bom select option:selected').attr('data-bom-id');
-            new Model('mrp.bom').call('get_diff_bom_data', [{target_bom_id:parseInt(target_bom_id),origin_bom_id:parseInt(origin_bom_id)}]).then(function (result) {
+            new Model('mrp.bom').call('get_diff_bom_data',[], {target_bom_id:parseInt(target_bom_id),origin_bom_id:parseInt(origin_bom_id)}).then(function (result) {
                 console.log(result);
-
+                $('.cost_matching_container tbody').html('');
+                $('.cost_matching_container tbody').append(QWeb.render('cost_matching_tbody_templ',{result:result, target:true}));
             })
         },
         //确认源bom的选择 渲染table表
@@ -36,14 +37,13 @@ odoo.define('linkloving_process_inherit.bom_cost_reproduce', function (require) 
             new Model('mrp.bom').call('get_bom_line_list', [[parseInt(bom_id)]]).then(function (result) {
                 console.log(result);
                 $('.cost_matching_container tbody').html('');
-                $('.cost_matching_container tbody').append(QWeb.render('cost_matching_tbody_templ',{result:result}));
+                $('.cost_matching_container tbody').append(QWeb.render('cost_matching_tbody_templ',{result:result, target:false}));
             })
         },
         //源bom、目标bom下的输入框搜索事件
         origin_bom_search_func: _.debounce (function(e) {
             var e = e || window.event;
             var target = e.target || e.srcElement;
-            console.log($(target).parents('td'));
             new Model('mrp.bom').call('get_bom_list', [{name: $(target).parents('td').find('input').val()}]).then(function (result) {
                 console.log(result);
                 $(target).parents('td').find('select').html('');
