d7e7fb00c47bb0ff2923fe9dc2e78ad3b73915e6
wangkefu <745376631@qq.com>
2018-04-07 12:49:33 +0800

    update

--- linkloving_process_inherit/static/src/js/bom_cost_reproduce.js
+++ linkloving_process_inherit/static/src/js/bom_cost_reproduce.js
@@ -15,13 +15,28 @@ odoo.define('linkloving_process_inherit.bom_cost_reproduce', function (require) 
         template: "cost_matching_templ",
         events: {
             'input .origin_bom input': 'origin_bom_search_func',
+            // 'blur .origin_bom input':'confirm_origin_bom_sel_func',
+            'click .origin_bom ul li':'confirm_origin_bom_sel_func'
         },
 
+        //确认源bom的选择 渲染table表
+        confirm_origin_bom_sel_func:function () {
+            var bom_id = $('.origin_bom select option:selected').attr('data-bom-id');
+            new Model('mrp.bom').call('get_bom_line_list', [[parseInt(bom_id)]]).then(function (result) {
+                console.log(result);
+
+            })
+        },
+        //源bom下的输入框搜索事件
         origin_bom_search_func: _.debounce (function(ev) {
             new Model('mrp.bom').call('get_bom_list', [[],{name:$('.origin_bom input').val()}]).then(function (result) {
                 console.log(result);
+                $('.origin_bom select').html('');
+                $.when($('.origin_bom select').append(QWeb.render('cost_matching_select_templ',{result:result}))).then(function () {
+                     $('.origin_bom select').selectpicker('refresh')
+                })
             })
-        },1000,true),
+        },300,true),
 
         init:function (parent, action) {
             this._super(parent);
