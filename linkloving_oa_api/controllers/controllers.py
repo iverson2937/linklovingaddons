dc27c27113049cf23e3aa54cb70d3ef80d9d015a
PeaceP <747780958@qq.com>
2018-05-28 17:43:20 +0800

    考勤模块更新

--- linkloving_oa_api/controllers/controllers.py
+++ linkloving_oa_api/controllers/controllers.py
@@ -2779,7 +2779,7 @@ class LinklovingOAApi(http.Controller):
                     "open_id": open_id,
                 })
 
-            domain = [("write_date", ">", day_start), ("write_date", "<", day_end), ("employee_id", "=", employee.id)]
+            domain = [("check_in", ">", day_start), ("check_in", "<", day_end), ("employee_id", "=", employee.id)]
 
             attendance_list = request.env['hr.attendance'].sudo().search(domain)
             data = []
