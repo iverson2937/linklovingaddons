428d3defa9d10ca6f18c12fdd2153b06b51cf0c6
allen <iverson2937@163.com>
2018-11-16 18:34:57 +0800

    update purchase

---
+++ linkloving_purchase/controllers/controllers.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+from odoo.http import request
+
+
+class RtOrderImport(http.Controller):
+    @http.route('/rt_web/get_purchase_order', auth='none', type='json', csrf=False, methods=['POST'])
+    def get_purchase_order(self, **kw):
+        print 'ddddddddddddddddddddddd'
+        db = request.jsonrequest.get("db")  # 所选账套
+        request.session.db = db  # 设置账套
+        request.params["db"] = db
+
+        vals = request.jsonrequest.get("po_number")  # 需要查询的产品数据
+        print vals
+
+        purchase_id = request.env["purchase.order"].sudo().search([("name", "=", vals)])
+        data_return = {}
+        try:
+            data_return.update({
+                'partner_id': purchase_id.partner_id.name,
+                'confirmation_date': purchase_id.confirmation_date,
+                'tax_id':purchase_id.tax_id.name,
+                'date_planned': purchase_id.handle_date,
+                'remark': purchase_id.remark,
+                'line_ids': [{
+                    'product_id': line.product_id.name,
+                    'product_uom_qty': line.product_uom_qty,
+                    'price_unit': line.price_unit,
+                    'tax_id': line.tax_id.name,
+                } for line in purchase_id.order_line]
+            })
+
+        except Exception, e:
+            return {
+                "code": -2,
+                "msg": u"出现异常 %s" % str(e)
+            }
+        return {
+            "code": 1,
+            'vals': data_return,
+        }
